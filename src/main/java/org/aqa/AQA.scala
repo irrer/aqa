5fd0a0bda3492dcdb5b64db1abd7567d436df24a
Jim Irrer <irrer@umich.edu>
2016-08-18 09:43:32 -0400

    Added better comment.

--- src/main/java/org/aqa/AQA.scala
+++ src/main/java/org/aqa/AQA.scala
@@ -3,16 +3,42 @@ package org.aqa
 import org.aqa.db.DbSetup
 import org.aqa.web.WebServer
 import org.aqa.run.Run
-import org.aqa.Config
+import edu.umro.ScalaUtil.PeriodicRestart
+import org.aqa.Logging._
 
+/**
+ * Main service entry point.
+ */
 object AQA {
     def main(args: Array[String]): Unit = {
-        println("Service starting")
-        if (Config.validate) {
-            DbSetup.init
-            Run.handleRunningProcedureList
-            new WebServer
+        try {
+            println("Service starting")
+            if (Config.validate) {
+                DbSetup.init
+                Run.handleRunningProcedureList
+                new WebServer
+                new PeriodicRestart(Config.RestartTime)
+                println("Service started")
+            }
         }
-        println("Service started")
+        catch {
+            // Exceptions thrown to this level should not happen, and if they do it probably means that something
+            // very unexpected has happened.
+            //
+            // If there is a problem, catch and log the error, delay, and then exit with a failed status.  The
+            // failed status will tell the service wrapper to restart the service.  The delay is there in the
+            // event that this service behaves badly and keeps exiting when started, an keeps the service from
+            // using excessive resources.
+            case e: Exception => {
+                logSevere("Unexpected exception in main: " + fmtEx(e))
+                Thread.sleep(30 * 1000)
+                System.exit(1)
+            }
+            case t: Throwable => {
+                logSevere("Unexpected throwable in main: " + fmtEx(t))
+                Thread.sleep(30 * 1000)
+                System.exit(2)
+            }
+        }
     }
 }
