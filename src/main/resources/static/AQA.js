29d3f5ce850584db7a0928e450ef94792e494ad4
Jim Irrer <irrer@umich.edu>
2018-12-19 18:01:31 -0500

    Added javascript for tranlating aliases to real names.

--- src/main/resources/static/AQA.js
+++ src/main/resources/static/AQA.js
@@ -44,3 +44,61 @@ $(document).ready(function() {
 	});
 	jQuery('time.timeago').timeago();
 });
+
+
+
+/*
+ * Translate all the aliased names in the current page to real names.
+ *  
+ * Retrieve the list of aliased entities (machines, users, 
+ * etc.) that the current user is allowed to view along with 
+ * their real names as a table.  Find all HTML elements with 
+ * the special attribute "AQAAlias", and replace their contents 
+ * (the alias) with the real name.   
+ *  
+ * If the contents of an element do not match a real name, then 
+ * ignore it.  This is usually because the user is viewing a 
+ * page with entities that they should not be allowed to see. 
+ *  
+ * Finally, set or append to the title of the element its alias 
+ * so the user can easily find it. 
+ */
+var jsonhttp = new XMLHttpRequest();
+var aliasToRealUrl = "aliasToRealList.json";  // defines the URL for getting the alias to real table
+
+jsonhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var aliasToRealList = JSON.parse(this.responseText);
+    myFunction(aliasToRealList);
+  }
+};
+jsonhttp.open("GET", aliasToRealUrl, true);
+jsonhttp.send();
+
+function myFunction(aliasToRealList) {
+  var publicList = $( "[AQAAlias]" ); // defines the attribute for showing real names
+
+  var arMap = new Map();
+  aliasToRealList.map(ar => arMap.set(ar.alias.trim(), ar.real.trim()));
+
+  var s = arMap.get("MACH_2");
+  var p;
+
+  for (p = 0; p < publicList.length; p++) {
+    var alias = publicList[p].innerHTML.trim();
+    var real = arMap.get(alias);
+    if (real != null) {
+      publicList[p].innerHTML = real;
+
+      var title = publicList[p].getAttribute("title");
+      var showAlias = "Alias: " + alias;
+      if (title == null) {
+        publicList[p].setAttribute("title", showAlias);
+      }
+      else {
+        publicList[p].setAttribute("title", title + " " + showAlias);
+      }
+    }
+  }
+
+}
