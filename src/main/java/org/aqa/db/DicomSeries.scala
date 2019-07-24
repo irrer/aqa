f6333a2143852e7513a01ba9c2944389a29ad67e
Jim Irrer <irrer@umich.edu>
2019-07-24 18:40:13 -0400

    Track user instead of institution.

--- src/main/java/org/aqa/db/DicomSeries.scala
+++ src/main/java/org/aqa/db/DicomSeries.scala
@@ -10,6 +10,7 @@ import java.sql.Timestamp
 import com.pixelmed.dicom.TagFromName
 import com.pixelmed.dicom.AttributeList
 import com.pixelmed.dicom.AttributeTag
+import edu.umro.ScalaUtil.DicomUtil
 
 /**
  * Store the contents of a DICOM series.
@@ -17,7 +18,7 @@ import com.pixelmed.dicom.AttributeTag
  */
 case class DicomSeries(
   dicomSeriesPK: Option[Long], // primary key
-  institutionPK: Long, // institution to which this file belongs
+  userPK: Long, // user that uploaded or created this series
   inputPK: Option[Long], // referenced input, if available
   machinePK: Option[Long], // referenced machine, if available
   sopInstanceUID: String, // DICOM SOPInstanceUID of first file in series.  First is defined by the one with the earliest <code>date</code>.
@@ -35,20 +36,21 @@ case class DicomSeries(
    * Insert, returning the row that was inserted.  Note that dicomSeriesPK in the return value is defined.
    */
   def insert: DicomSeries = {
-
     val insertQuery = DicomSeries.query returning DicomSeries.query.map(_.dicomSeriesPK) into ((dicomSeries, dicomSeriesPK) => dicomSeries.copy(dicomSeriesPK = Some(dicomSeriesPK)))
 
     val action = insertQuery += this
     val result = Db.run(action)
     result
   }
+
+  lazy val attributeListList = DicomUtil.zippedByteArrayToDicom(content)
 }
 
 object DicomSeries extends Logging {
   class DicomSeriesTable(tag: Tag) extends Table[DicomSeries](tag, "dicomSeries") {
 
     def dicomSeriesPK = column[Long]("dicomSeriesPK", O.PrimaryKey, O.AutoInc)
-    def institutionPK = column[Long]("institutionPK")
+    def userPK = column[Long]("userPK")
     def inputPK = column[Option[Long]]("inputPK")
     def machinePK = column[Option[Long]]("machinePK")
     def sopInstanceUID = column[String]("sopInstanceUID")
@@ -63,7 +65,7 @@ object DicomSeries extends Logging {
 
     def * = (
       dicomSeriesPK.?,
-      institutionPK,
+      userPK,
       inputPK,
       machinePK,
       sopInstanceUID,
@@ -77,7 +79,7 @@ object DicomSeries extends Logging {
       content) <> ((DicomSeries.apply _)tupled, DicomSeries.unapply _)
 
     def dicomSeriesFK = foreignKey("dicomSeriesPK", dicomSeriesPK, DicomSeries.query)(_.dicomSeriesPK, onDelete = ForeignKeyAction.Cascade, onUpdate = ForeignKeyAction.Cascade)
-    def institutionFK = foreignKey("institutionPK", institutionPK, DicomSeries.query)(_.institutionPK, onDelete = ForeignKeyAction.Cascade, onUpdate = ForeignKeyAction.Cascade)
+    def institutionFK = foreignKey("userPK", userPK, DicomSeries.query)(_.userPK, onDelete = ForeignKeyAction.Cascade, onUpdate = ForeignKeyAction.Cascade)
     def machineFK = foreignKey("machinePK", machinePK, DicomSeries.query)(_.machinePK, onDelete = ForeignKeyAction.Cascade, onUpdate = ForeignKeyAction.Cascade)
   }
 
@@ -111,7 +113,7 @@ object DicomSeries extends Logging {
     Db.run(action)
   }
 
-  def makeDicomSeries(instPK: Option[Long], inpPK: Option[Long], machPK: Option[Long], alList: Seq[AttributeList]): DicomSeries = {
+  def makeDicomSeries(usrPK: Long, inpPK: Option[Long], machPK: Option[Long], alList: Seq[AttributeList]): DicomSeries = {
 
     val datePatID = alList.map(al => Util.extractDateTimeAndPatientIdFromDicom(al))
 
@@ -128,18 +130,6 @@ object DicomSeries extends Logging {
       if (s == null) None else Some(s)
     }
 
-    def getInstitutionPK: Long = {
-      (instPK.isDefined, machPK.isDefined, inpPK.isDefined) match {
-        case (true, _, _) => instPK.get // if given, use it
-        case (_, true, _) => Some(Machine.get(machPK.get).get.institutionPK).get // if machine is given, then get institution from it
-        case (_, _, true) => { // if input is given, then get institution from it via user
-          val userPK = Input.get(inpPK.get).get.userPK.get
-          Some(User.get(userPK).get.institutionPK).get
-        }
-        case _ => throw new IllegalArgumentException("Could not find institution for construction of DicomSeries")
-      }
-    }
-
     def getInputPK = inpPK
 
     def getMachinePK = {
@@ -151,31 +141,19 @@ object DicomSeries extends Logging {
     }
 
     def getSopInstanceUID = byTag(TagFromName.SOPInstanceUID).get
-
     def getSeriesInstanceUID = byTag(TagFromName.SeriesInstanceUID).get
-
     def getFrameOfReferenceUID = byTag(TagFromName.FrameOfReferenceUID)
-
     def getModality = byTag(TagFromName.Modality).get
-
     def getSopClassUID = byTag(TagFromName.SOPClassUID).get
-
-    def getDate = {
-      new Timestamp(sorted.head.date.getTime)
-    }
-
+    def getDate = new Timestamp(sorted.head.date.getTime)
     def getPatientID = sorted.map(_.patId).flatten.headOption
-
     def getSize = sorted.size
-
-    def getContent: Array[Byte] = {
-      ???
-    }
+    def getContent: Array[Byte] = DicomUtil.dicomToZippedByteArray(alList)
 
     new DicomSeries(
       None,
-      getInstitutionPK,
-      getInputPK,
+      usrPK,
+      inpPK,
       getMachinePK,
       getSopInstanceUID,
       getSeriesInstanceUID,
