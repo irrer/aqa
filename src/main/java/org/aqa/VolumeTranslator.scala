72d1b2d65362add328e88271b1b061fc52f09792
Jim Irrer <irrer@umich.edu>
2019-08-09 13:55:13 -0400

    fixed cut and paste bug

--- src/main/java/org/aqa/VolumeTranslator.scala
+++ src/main/java/org/aqa/VolumeTranslator.scala
@@ -40,8 +40,8 @@ class VolumeTranslator(alList: Seq[AttributeList]) {
 
   def mm2vox(point: Point3d): Point3d = {
     val x = (point.getX - ImagePositionPatient(0)) / voxSize(0)
-    val y = (point.getY - ImagePositionPatient(1)) / voxSize(0)
-    val z = (point.getZ - ImagePositionPatient(2)) / voxSize(0)
+    val y = (point.getY - ImagePositionPatient(1)) / voxSize(1)
+    val z = (point.getZ - ImagePositionPatient(2)) / voxSize(2)
     new Point3d(x, y, z)
   }
 
