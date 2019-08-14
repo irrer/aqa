3bddefa7fc9c05b3fcd73471b067ead23df894e8
Jim Irrer <irrer@umich.edu>
2019-08-14 16:47:43 -0400

    Wrote BBbyEPIDRunReq.  Not yet tested.

--- src/main/java/org/aqa/webrun/bbByEpid/BBbyEPIDRunReq.scala
+++ src/main/java/org/aqa/webrun/bbByEpid/BBbyEPIDRunReq.scala
@@ -1,5 +1,37 @@
 package org.aqa.webrun.bbByEpid
 
-class BBbyEPIDRunReq {
-  
+import org.aqa.DicomFile
+import com.pixelmed.dicom.AttributeList
+import org.aqa.ImageRegistration
+import org.aqa.webrun.bbByCBCT.BBbyCBCTRunReq
+import java.io.File
+import org.aqa.db.Machine
+
+object BBbyEPIDRunReq {
+  case class ImageReg(epidFile: DicomFile, regFile: DicomFile) {
+    val epid = epidFile.attributeList.get
+    val reg = regFile.attributeList.get
+    val imageRegistration = new ImageRegistration(reg)
+
+    def reDir(dir: File) = new ImageReg(epidFile.reDir(dir), regFile.reDir(dir))
+  }
+}
+
+case class BBbyEPIDRunReq(rtplanDicomFile: Either[DicomFile, AttributeList], imageReg: Seq[BBbyEPIDRunReq.ImageReg], machine: Machine) {
+
+  val rtplan = if (rtplanDicomFile.isLeft) rtplanDicomFile.left.get.attributeList.get else rtplanDicomFile.right.get
+
+  def reDir(dir: File): BBbyEPIDRunReq = {
+    val origDirPath = imageReg.head.epidFile.file.getParentFile.getAbsolutePath
+    def ifRedir(df: DicomFile) = if (df.file.getParentFile.getAbsolutePath.equalsIgnoreCase(origDirPath)) df.reDir(dir) else df
+
+    val imageReg2 = imageReg.map(ir => ir.reDir(dir))
+
+    val plan2 = if (rtplanDicomFile.isLeft)
+      Left(rtplanDicomFile.left.get.reDir(dir))
+    else
+      Right(rtplan)
+
+    new BBbyEPIDRunReq(plan2, imageReg2, machine)
+  }
 }
