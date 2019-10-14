74c28e5b9888c3b49da6c2a775af0962070bc4bb
Jim Irrer <irrer@umich.edu>
2019-10-14 19:12:21 -0400

    re-enabled parallel processing

--- src/main/java/org/aqa/webrun/phase2/symmetryAndFlatness/SymmetryAndFlatnessAnalysis.scala
+++ src/main/java/org/aqa/webrun/phase2/symmetryAndFlatness/SymmetryAndFlatnessAnalysis.scala
@@ -388,8 +388,7 @@ object SymmetryAndFlatnessAnalysis extends Logging {
       val beamNameList = Config.SymmetryAndFlatnessBeamList.filter(beamName => runReq.derivedMap.contains(beamName))
 
       // only process beams that are both configured and have been uploaded
-      val resultList = beamNameList.par.map(beamName => analyze(beamName, extendedData, runReq, collimatorCentering)).toList  // TODO put back in
-      // val resultList = beamNameList.map(beamName => analyze(beamName, extendedData, runReq, collimatorCentering)).toList // TODO rm
+      val resultList = beamNameList.par.map(beamName => analyze(beamName, extendedData, runReq, collimatorCentering)).toList
 
       val pass = {
         0 match {
