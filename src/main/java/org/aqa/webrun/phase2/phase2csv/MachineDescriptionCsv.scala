46edcf71c8c7d7efe59b26a561f80bfc881e63ae
Jim Irrer <irrer@umich.edu>
2021-03-26 20:07:28 -0400

    Tweaks to CSV generation.
    Fixed URL to view results.

--- src/main/java/org/aqa/webrun/phase2/phase2csv/MachineDescriptionCsv.scala
+++ src/main/java/org/aqa/webrun/phase2/phase2csv/MachineDescriptionCsv.scala
@@ -1,18 +1,22 @@
 package org.aqa.webrun.phase2.phase2csv
 
 import org.aqa.Config
-import org.aqa.Util
 import org.aqa.db.Output
+import org.aqa.web.WebServer
 
 class MachineDescriptionCsv(metadataCache: MetadataCache) {
 
   private case class PrefixCol(header: String, toText: Output => String) {}
 
+  private def urlOfOutput(output: Output) : String = {
+    Config.RootUrl + WebServer.urlOfResultsFile(output.dir) + "/" + Output.displayFilePrefix + ".html"
+  }
+
   private val colList = Seq(
     PrefixCol("Type", (o: Output) => metadataCache.machineTypeMap(metadataCache.machineMap(o.machinePK.get).machineTypePK)),
     PrefixCol("Collimator", (o: Output) => metadataCache.collimatorTypeMap(metadataCache.machineMap(o.machinePK.get).multileafCollimatorPK)),
     PrefixCol("EPID", (o: Output) => metadataCache.epidTypeMap(metadataCache.machineMap(o.machinePK.get).epidPK)),
-    PrefixCol("URL", (o: Output) => Config.RootUrl + "/view/ViewOutput?outputPK=" + o.outputPK.get),
+    PrefixCol("URL", (o: Output) => urlOfOutput(o)),
     PrefixCol("Uploaded By", (o: Output) => metadataCache.userMap(o.userPK.get))
   )
 
--- src/main/java/org/aqa/webrun/phase2/phase2csv/SymmetryAndFlatnessCsv.scala
+++ src/main/java/org/aqa/webrun/phase2/phase2csv/SymmetryAndFlatnessCsv.scala
@@ -36,6 +36,7 @@ class SymmetryAndFlatnessCsv extends Phase2Csv[SymmetryAndFlatness.SymmetryAndFl
 
   override protected def makeColList: Seq[Col] = {
     Seq(
+      Col("Beam Name", (sf: SF) => sf.symmetryAndFlatness.beamName),
       Col("Axial Symmetry", (sf: SF) => sf.symmetryAndFlatness.axialSymmetry),
       Col("Transverse Symmetry", (sf: SF) => sf.symmetryAndFlatness.transverseSymmetry),
       Col("Flatness", (sf: SF) => sf.symmetryAndFlatness.flatness),
@@ -46,7 +47,6 @@ class SymmetryAndFlatnessCsv extends Phase2Csv[SymmetryAndFlatness.SymmetryAndFl
       Col("Left CU", (sf: SF) => sf.symmetryAndFlatness.left_cu),
       Col("Right CU", (sf: SF) => sf.symmetryAndFlatness.right_cu),
       Col("Center CU", (sf: SF) => sf.symmetryAndFlatness.center_cu),
-      Col("Beam Name", (sf: SF) => sf.symmetryAndFlatness.beamName),
       Col("Baseline Acquisition", (sf: SF) => Util.standardDateFormat.format(sf.baselineOutput.dataDate.get)),
       Col("Baseline Analysis", (sf: SF) => Util.standardDateFormat.format(sf.baselineOutput.startDate)),
       Col("Baseline Top CU", (sf: SF) => sf.baseline.top_cu),
