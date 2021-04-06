4306f82106f7e3ef0f652a4696981552d55d4008
Jim Irrer <irrer@umich.edu>
2021-04-06 17:19:20 -0400

    Cleaned up code.

--- src/test/java/aqa/test/TestVMATAnalysis.scala
+++ src/test/java/aqa/test/TestVMATAnalysis.scala
@@ -1,24 +1,25 @@
+package aqa.test
 
-package aqa.test;
-
+import com.pixelmed.dicom.AttributeList
+import edu.umro.ImageUtil.DicomImage
+import org.aqa.Config
+import org.aqa.DicomFile
+import org.aqa.Util
+import org.aqa.db.CollimatorCentering
+import org.aqa.db.VMAT
+import org.aqa.run.ProcedureStatus
+import org.aqa.webrun.phase2.Phase2Util
+import org.aqa.webrun.phase2.vmat.VMATAnalysis
+import org.scalatest.Assertion
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
-import java.io.File
-import org.aqa.Config
-import org.aqa.Util
-import com.pixelmed.dicom.AttributeList
-import org.aqa.DicomFile
-import org.aqa.webrun.phase2.vmat.VMATAnalysis
-import org.aqa.webrun.phase2.Phase2Util
-import org.aqa.db.CollimatorCentering
-import org.aqa.run.ProcedureStatus
-import edu.umro.ImageUtil.DicomImage
-import org.aqa.db.VMAT
+
 import java.awt.Rectangle
+import java.io.File
 
 /**
- * Test VMAT analysis.
- */
+  * Test VMAT analysis.
+  */
 
 class TestVMATAnalysis extends FlatSpec with Matchers {
 
@@ -26,11 +27,11 @@ class TestVMATAnalysis extends FlatSpec with Matchers {
 
   (0 to 5).map(_ => println("-----------------------------------------------------------------------------------------------------"))
 
-  private def testVmatPair(vmatPair: Config.VMATBeamPair, rtplan: AttributeList, imageList: Seq[DicomFile]) = {
+  private def testVmatPair(vmatPair: Config.VMATBeamPair, rtplan: AttributeList, imageList: Seq[DicomFile]): Unit = {
     println("Testing VMAT beam pair: " + vmatPair)
     def isTheNamedBeam(img: AttributeList, name: String): Boolean = {
       val n = Phase2Util.getBeamNameOfRtimage(rtplan, img)
-      n.isDefined && (n.get.equals(name))
+      n.isDefined && n.get.equals(name)
     }
 
     val mlc = imageList.find(df => isTheNamedBeam(df.attributeList.get, vmatPair.MLC)).get.attributeList.get
@@ -38,21 +39,17 @@ class TestVMATAnalysis extends FlatSpec with Matchers {
 
     if (false) { // TODO rm
       println
-      println((new DicomImage(mlc).getSubimage(new Rectangle(1190 - 50, 1190 - 40, 50, 50))).pixelsToText)
+      println(new DicomImage(mlc).getSubimage(new Rectangle(1190 - 50, 1190 - 40, 50, 50)).pixelsToText)
       println
-      println((new DicomImage(open).getSubimage(new Rectangle(1190 - 50, 1190 - 40, 50, 50))).pixelsToText)
+      println(new DicomImage(open).getSubimage(new Rectangle(1190 - 50, 1190 - 40, 50, 50)).pixelsToText)
       System.exit(99)
     }
 
-    val aoiList = VMATAnalysis.testGetPlanAoiList(vmatPair, mlc, open, rtplan)
+    val aoiList = VMATAnalysis.testGetPlanAoiList(vmatPair, rtplan)
     println("AOI List: \n    " + aoiList.mkString("\n    "))
 
     // fake centering with offsets at 0
-    val collimatorCentering = new CollimatorCentering(None, -1, ProcedureStatus.running.toString,
-      "sop090", "sop270",
-      0, 0,
-      0, 0, 0, 0,
-      0, 0, 0, 0)
+    val collimatorCentering = new CollimatorCentering(None, -1, ProcedureStatus.running.toString, "sop090", "sop270", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     val outputPK: Long = -1
     val vmatSeq = VMATAnalysis.testAnalyze(vmatPair, mlc, open, rtplan, collimatorCentering, outputPK, new DicomImage(mlc), new DicomImage(open))
 
@@ -74,17 +71,15 @@ class TestVMATAnalysis extends FlatSpec with Matchers {
     val rtplan = dicomFileList.find(df => Util.modalityOfAl(df.attributeList.get).equals("RTPLAN")).get.attributeList.get
     val imageList = dicomFileList.filter(df => Util.modalityOfAl(df.attributeList.get).equals("RTIMAGE"))
 
-    Config.VMATBeamPairList.map(vmatPair => testVmatPair(vmatPair, rtplan, imageList))
+    Config.VMATBeamPairList.foreach(vmatPair => testVmatPair(vmatPair, rtplan, imageList))
 
-    (true) should be(true)
+    true should be(true)
   }
 
-  def runTest = {
-
-    val inDir = new File("""target\TestVMATAnalysis""")
+  def runTest: Assertion = {
 
     // list of directories that contain a VMAT set for analysis
-    val dirList = (new File("""src\test\resources\TestVMAT""")).listFiles
+    val dirList = new File("""src\test\resources\TestVMAT""").listFiles
     println("List of VMAT directories used as input:\n    " + dirList.map(dir => dir.getAbsolutePath).mkString("\n    "))
 
     dirList.map(dir => testDir(dir))
