b412f03b2390b7fa15dfe75d96de1678aa447364
Jim Irrer <irrer@umich.edu>
2021-04-29 07:48:42 -0400

    Cleaned up code.

--- src/main/java/org/aqa/web/UserList.scala
+++ src/main/java/org/aqa/web/UserList.scala
@@ -1,32 +1,31 @@
 package org.aqa.web
 
-import org.aqa.db.User
-import org.aqa.db.Institution
-import org.restlet.Response
-import org.aqa.db.User.UserInstitution
-import org.aqa.web.WebUtil._
 import org.aqa.AnonymizeUtil
 import org.aqa.db.CachedUser
+import org.aqa.db.User
+import org.aqa.db.User.UserInstitution
+import org.aqa.web.WebUtil._
+import org.restlet.Response
 
 object UserList {
   private val path = new String((new UserList).pathOf)
 
-  def redirect(response: Response) = response.redirectSeeOther(path)
+  def redirect(response: Response): Unit = response.redirectSeeOther(path)
 }
 
 class UserList extends GenericList[UserInstitution] with WebUtil.SubUrlAdmin {
 
   override val listName = "User"
 
-  private val idCol = new Column[UserInstitution]("Id", _.user.id, (ui) => makePrimaryKeyHtmlWithAQAAlias(ui.user.id, ui.user.userPK))
+  private val idCol = new Column[UserInstitution]("Id", _.user.id, ui => makePrimaryKeyHtmlWithAQAAlias(ui.user.id, ui.user.userPK))
 
-  private val nameCol = encryptedColumn("Name", AnonymizeUtil.userAliasFullNamePrefixId, (ui) => ui.user.userPK.get)
+  private val nameCol = encryptedColumn("Name", AnonymizeUtil.userAliasFullNamePrefixId, ui => ui.user.userPK.get)
 
-  private val emailCol = encryptedColumn("Email", AnonymizeUtil.userAliasEmailPrefixId, (ui) => ui.user.userPK.get)
+  private val emailCol = encryptedColumn("Email", AnonymizeUtil.userAliasEmailPrefixId, ui => ui.user.userPK.get)
 
-  private val institutionCol = new Column[UserInstitution]("Institution", _.institution.name, (ui) => wrapAlias(ui.institution.name))
+  private val institutionCol = new Column[UserInstitution]("Institution", _.institution.name, ui => wrapAlias(ui.institution.name))
 
-  private def optionToString[T](opt: Option[T]): String = if (opt.isDefined) opt.get.toString else "none";
+  private def optionToString[T](opt: Option[T]): String = if (opt.isDefined) opt.get.toString else "none"
 
   def getRole(ui: UserInstitution): String = ui.user.role
 
@@ -34,7 +33,7 @@ class UserList extends GenericList[UserInstitution] with WebUtil.SubUrlAdmin {
 
   override val columnList = Seq(idCol, nameCol, emailCol, institutionCol, roleCol)
 
-  override def getData(valueMap: ValueMapT, response: Response) = {
+  override def getData(valueMap: ValueMapT, response: Response): Seq[UserInstitution] = {
     val v = valueMap.get(checkbox.label)
     val all = v.isDefined && (v.get.equalsIgnoreCase("true") || v.get.equalsIgnoreCase("on"))
     val instPK = {
@@ -50,16 +49,17 @@ class UserList extends GenericList[UserInstitution] with WebUtil.SubUrlAdmin {
 
   override def getPK(value: UserInstitution): Long = value.user.userPK.get
 
-  private def makeButton(name: String, buttonType: ButtonType.Value): FormButton = {
+  private def makeButton(buttonType: ButtonType.Value): FormButton = {
+    val name = "Refresh"
     val action = pathOf + "?" + name + "=" + name
     new FormButton(name, 1, 0, subUrl, action, buttonType)
   }
 
   val checkbox = new WebInputCheckbox("All Institutions", true, Some("Check to show users from all institutions, then click 'Refresh'"), 2, 0)
-  val refresh = makeButton("Refresh", ButtonType.BtnPrimary)
+  val refresh: FormButton = makeButton(ButtonType.BtnPrimary)
 
   override def htmlFieldList(valueMap: ValueMapT): List[WebRow] = {
     val webRow = new WebRow(List(checkbox, refresh))
-    List(webRow);
+    List(webRow)
   }
 }
