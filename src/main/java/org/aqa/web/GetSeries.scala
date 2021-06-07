5eed26d301caaca95152fb2eba021b5836507d1d
Jim Irrer <irrer@umich.edu>
2021-06-07 16:03:59 -0400

    Added synchronization so that only one request would be processed at a time.

--- src/main/java/org/aqa/web/GetSeries.scala
+++ src/main/java/org/aqa/web/GetSeries.scala
@@ -1,32 +1,35 @@
 package org.aqa.web
 
-import org.restlet.Restlet
-import org.restlet.Request
-import org.restlet.Response
-import scala.xml.Elem
-import scala.xml.PrettyPrinter
-import org.restlet.data.Status
-import org.restlet.data.MediaType
-import WebUtil._
+import com.pixelmed.dicom.AttributeFactory
+import com.pixelmed.dicom.AttributeTag
+import com.pixelmed.dicom.TagFromName
+import org.aqa.AnonymizeUtil
 import org.aqa.Logging
 import org.aqa.Util
-import java.io.File
-import org.aqa.AnonymizeUtil
-import org.aqa.db.User
-import com.pixelmed.dicom.TagFromName
 import org.aqa.db.DicomAnonymous
-import com.pixelmed.dicom.AttributeFactory
 import org.aqa.db.DicomSeries
-import com.pixelmed.dicom.AttributeTag
-import scala.xml.XML
-import edu.umro.ScalaUtil.DicomUtil
-import edu.umro.ScalaUtil.Trace
 import org.aqa.db.Output
 import org.aqa.db.Procedure
+import org.aqa.db.User
+import org.aqa.web.WebUtil._
+import org.restlet.Request
+import org.restlet.Response
+import org.restlet.Restlet
+import org.restlet.data.MediaType
+import org.restlet.data.Status
+
+import java.io.File
+import scala.xml.Elem
+import scala.xml.PrettyPrinter
+import scala.xml.XML
+
+object GetSeries {
+  private val sync = "sync"
+}
 
 /**
- * Generate XML that lists DICOM series associated with the given patient ID.
- */
+  * Generate XML that lists DICOM series associated with the given patient ID.
+  */
 class GetSeries extends Restlet with SubUrlRoot with Logging {
 
   private val PatientIDTag = "PatientID"
@@ -71,14 +74,14 @@ class GetSeries extends Restlet with SubUrlRoot with Logging {
     val daList = dicomAnonList.map(da => (daKey(da.institutionPK, da.attributeTag, da.value), da)).toMap
 
     /**
-     * Given a tag and anonymized value, return the non-anonymized value.
-     */
+      * Given a tag and anonymized value, return the non-anonymized value.
+      */
     def lookup(tag: AttributeTag, anonValue: String): Option[String] = {
       val tagText = DicomAnonymous.formatAnonAttributeTag(tag)
 
       daList.get(daKey(institutionPK, DicomAnonymous.formatAnonAttributeTag(tag), anonValue)) match {
         case Some(dicomAnonymous) => Some(AnonymizeUtil.decryptWithNonce(institutionPK, dicomAnonymous.value_real))
-        case _ => None
+        case _                    => None
       }
     }
 
@@ -86,12 +89,12 @@ class GetSeries extends Restlet with SubUrlRoot with Logging {
     val procedureList = Procedure.list
 
     /**
-     * Lookup the name+version of a procedure
-     */
+      * Lookup the name+version of a procedure
+      */
     def nameOfProcedure(procedurePK: Long) = {
       procedureList.find(p => p.procedurePK.get == procedurePK) match {
         case Some(proc) => proc.name + " " + proc.version
-        case _ => "unknown"
+        case _          => "unknown"
       }
     }
 
@@ -118,49 +121,37 @@ class GetSeries extends Restlet with SubUrlRoot with Logging {
       val serInstUid = getOpt(Some(dicomSeries.seriesInstanceUID), TagFromName.SeriesInstanceUID, "SeriesInstanceUID")
       val frmOfRef = getOpt(dicomSeries.frameOfReferenceUID, TagFromName.FrameOfReferenceUID, "FrameOfReferenceUID")
       val mappedFrameOfReferenceUID = getOpt(dicomSeries.mappedFrameOfReferenceUID, TagFromName.FrameOfReferenceUID, "mappedFrameOfReferenceUID")
-      val modality = Some(<Modality>{ dicomSeries.modality }</Modality>)
-      val sopClassUid = Some(<SOPClassUID>{ dicomSeries.sopClassUID }</SOPClassUID>)
+      val modality = Some(<Modality>{dicomSeries.modality}</Modality>)
+      val sopClassUid = Some(<SOPClassUID>{dicomSeries.sopClassUID}</SOPClassUID>)
       val devSerNo = getOpt(dicomSeries.deviceSerialNumber, TagFromName.DeviceSerialNumber, "DeviceSerialNumber")
       val patId = getOpt(dicomSeries.patientID, TagFromName.PatientID, "PatientID")
       val referencedRtplanUID = getOpt(dicomSeries.referencedRtplanUID, TagFromName.ReferencedSOPInstanceUID, "referencedRtplanUID")
       val startDate = {
-        if (output.isDefined) Some(<AnalysisStartDate>{ Util.standardDateFormat.format(output.get.startDate) }</AnalysisStartDate>)
+        if (output.isDefined) Some(<AnalysisStartDate>{Util.standardDateFormat.format(output.get.startDate)}</AnalysisStartDate>)
         else None
       }
       val procedure = {
-        if (output.isDefined) Some(<Procedure>{ nameOfProcedure(output.get.procedurePK) }</Procedure>)
+        if (output.isDefined) Some(<Procedure>{nameOfProcedure(output.get.procedurePK)}</Procedure>)
         else None
       }
       val status = {
-        if (output.isDefined) Some(<Status>{ output.get.status }</Status>)
+        if (output.isDefined) Some(<Status>{output.get.status}</Status>)
         else None
       }
       val url = {
         urlOfDicomSeries(dicomSeries) match {
-          case Some(u) => Some(<URL>{ u }</URL>)
-          case _ => None
+          case Some(u) => Some(<URL>{u}</URL>)
+          case _       => None
         }
       }
 
-      val elemList: Seq[Elem] = Seq(
-        serInstUid,
-        devSerNo,
-        frmOfRef,
-        mappedFrameOfReferenceUID,
-        modality,
-        sopClassUid,
-        patId,
-        referencedRtplanUID,
-        url,
-        procedure,
-        status,
-        startDate).flatten
+      val elemList: Seq[Elem] = Seq(serInstUid, devSerNo, frmOfRef, mappedFrameOfReferenceUID, modality, sopClassUid, patId, referencedRtplanUID, url, procedure, status, startDate).flatten
 
       val seriesXml = {
         <Series>
-          { elemList }
-          <DataDate>{ Util.standardDateFormat.format(dicomSeries.date) }</DataDate>
-          <NumberOfSlices>{ dicomSeries.size }</NumberOfSlices>
+          {elemList}
+          <DataDate>{Util.standardDateFormat.format(dicomSeries.date)}</DataDate>
+          <NumberOfSlices>{dicomSeries.size}</NumberOfSlices>
         </Series>
       }
 
@@ -170,10 +161,10 @@ class GetSeries extends Restlet with SubUrlRoot with Logging {
     val seriesListXml = {
       <SeriesList>
         {
-          val groupSize = Math.min(5, dicomSeriesList.size / 8)
-          val partitioned = edu.umro.ScalaUtil.Util.sizedGroups(dicomSeriesList, groupSize)
-          partitioned.par.map(sub => sub.map(ds => toXml(ds))).toList.flatten
-        }
+        val groupSize = Math.min(5, dicomSeriesList.size / 8)
+        val partitioned = edu.umro.ScalaUtil.Util.sizedGroups(dicomSeriesList, groupSize)
+        partitioned.par.map(sub => sub.map(ds => toXml(ds))).toList.flatten
+      }
       </SeriesList>
     }
 
@@ -196,15 +187,17 @@ class GetSeries extends Restlet with SubUrlRoot with Logging {
       val realPatientId = valueMap.get(PatientIDTag)
 
       0 match {
-        case _ if (user.isEmpty) => badRequest(response, "User not logged in or user can not be identified", Status.CLIENT_ERROR_BAD_REQUEST)
+        case _ if (user.isEmpty)          => badRequest(response, "User not logged in or user can not be identified", Status.CLIENT_ERROR_BAD_REQUEST)
         case _ if (realPatientId.isEmpty) => badRequest(response, "No " + PatientIDTag + " value given", Status.CLIENT_ERROR_BAD_REQUEST)
-        case _ => {
-          logger.info("Getting list of series for PatientID " + realPatientId.get)
-          val xml = generateXml(user.get, realPatientId.get)
-          val xmlText = new PrettyPrinter(1024, 2).format(xml)
-          response.setEntity(xmlText, MediaType.TEXT_XML)
-          logger.info("Got list of series for PatientID " + realPatientId.get)
-        }
+        case _ =>
+          // only allow one request at a time to avoid overloading the server
+          GetSeries.sync.synchronized {
+            logger.info("Getting list of series for PatientID " + realPatientId.get)
+            val xml = generateXml(user.get, realPatientId.get)
+            val xmlText = new PrettyPrinter(1024, 2).format(xml)
+            response.setEntity(xmlText, MediaType.TEXT_XML)
+            logger.info("Got list of series for PatientID " + realPatientId.get)
+          }
       }
 
     } catch {
