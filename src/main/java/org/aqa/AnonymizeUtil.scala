590f4996b3fa9a3415f1a09f4a0a1f169f56e1ff
Jim Irrer <irrer@umich.edu>
2021-06-17 16:18:08 -0400

    code cleanup

--- src/main/java/org/aqa/AnonymizeUtil.scala
+++ src/main/java/org/aqa/AnonymizeUtil.scala
@@ -1,16 +1,17 @@
 package org.aqa
 
-import java.io.File
-import scala.xml.XML
-import scala.collection.mutable.ArrayBuffer
-import org.aqa.web.WebUtil
-import edu.umro.ScalaUtil.FileUtil
-import org.aqa.db.Institution
-import scala.collection.Seq
-import edu.umro.ScalaUtil.DicomUtil
-import com.pixelmed.dicom.AttributeList
 import com.pixelmed.dicom.Attribute
+import com.pixelmed.dicom.AttributeList
+import edu.umro.ScalaUtil.DicomUtil
+import edu.umro.ScalaUtil.FileUtil
 import org.aqa.db.DicomAnonymous
+import org.aqa.db.Institution
+import org.aqa.web.WebUtil
+
+import java.io.File
+import scala.collection.Seq
+import scala.collection.mutable.ArrayBuffer
+import scala.xml.XML
 
 /** Utilities to support database anonymization. */
 
@@ -40,8 +41,8 @@ object AnonymizeUtil extends Logging {
   private val credentialFileNameSuffix = ".xml"
 
   /**
-   * Given the public (alias) name of the institution, construct a file.
-   */
+    * Given the public (alias) name of the institution, construct a file.
+    */
   private def credentialFile(name: String): File = {
     val safeName = FileUtil.replaceInvalidFileNameCharacters(name, '_').replace(' ', '_')
     new File(securityDir, credentialFileNamePrefix + safeName + credentialFileNameSuffix)
@@ -56,12 +57,12 @@ object AnonymizeUtil extends Logging {
   private val cacheTimeout = 10 * 60 * 1000
 
   /**
-   * For caching institution credentials.
-   *
-   * @param institution: Entry from database.
-   *
-   * @param key: Secret key as a hexadecimal string.
-   */
+    * For caching institution credentials.
+    *
+    * @param institution: Entry from database.
+    *
+    * @param key: Secret key as a hexadecimal string.
+    */
   private case class InstitutionCredentials(institution: Institution, key: String) {
 
     val aliasName: String = {
@@ -71,11 +72,11 @@ object AnonymizeUtil extends Logging {
 
     private lazy val cipher = Crypto.getCipher(key)
 
-    lazy val name_real = if (institution.name_real.isDefined) Crypto.decryptWithNonce(institution.name_real.get, cipher) else institution.name
+    lazy val name_real: String = if (institution.name_real.isDefined) Crypto.decryptWithNonce(institution.name_real.get, cipher) else institution.name
 
     /** To avoid keeping credentials in memory longer than necessary, give them an expiration time when they are removed from memory. */
     private val timeout = System.currentTimeMillis + cacheTimeout
-    def isValid = {
+    def isValid: Boolean = {
       timeout >= System.currentTimeMillis
     }
 
@@ -84,18 +85,18 @@ object AnonymizeUtil extends Logging {
     def decrypt(clearText: String): String = Crypto.decryptWithNonce(clearText, cipher)
 
     /**
-     * Save as XML file.  The XML file is local to the server and by design not in the database.
-     */
+      * Save as XML file.  The XML file is local to the server and by design not in the database.
+      */
     def persist(): Unit = {
       val file = credentialFile(aliasName)
       try {
         if (!securityDir.isDirectory) securityDir.mkdirs
         val xml = {
           <InstitutionCredentials>
-            <name>{ aliasName }</name>
-            <name_real>{ name_real }</name_real>
-            <creationTime>{ Util.standardDateFormat.format(System.currentTimeMillis) }</creationTime>
-            <key>{ key }</key>
+            <name>{aliasName}</name>
+            <name_real>{name_real}</name_real>
+            <creationTime>{Util.standardDateFormat.format(System.currentTimeMillis)}</creationTime>
+            <key>{key}</key>
           </InstitutionCredentials>
         }
         val xmlText = WebUtil.xmlToText(xml)
@@ -103,9 +104,8 @@ object AnonymizeUtil extends Logging {
         Util.writeBinaryFile(file, xmlText.getBytes)
         logger.info("Wrote credential file " + file.getAbsolutePath)
       } catch {
-        case t: Throwable => {
+        case t: Throwable =>
           logger.warn("Unable to write credential file " + file.getAbsolutePath + " : " + t + "\n" + fmtEx(t))
-        }
       }
     }
   }
@@ -115,70 +115,67 @@ object AnonymizeUtil extends Logging {
     def readCredentialFile(file: File): Option[InstitutionCredentials] = {
       try {
         val doc = XML.loadFile(file)
-        val name = (doc \ "name").head.text.toString
-        val key = (doc \ "key").head.text.toString
+        val name = (doc \ "name").head.text
+        val key = (doc \ "key").head.text
 
         Institution.getInstitutionByName(name) match {
-          case Some(institution) => Some(new InstitutionCredentials(institution, key))
-          case _ => {
+          case Some(institution) => Some(InstitutionCredentials(institution, key))
+          case _ =>
             // TODO this code will be obsolete when anonymization is complete, but until then
             // assume that the primary key was used to construct the institution's alias.
 
             val instPK = name.replaceAll(".*_", "").toLong
             val institution = Institution.get(instPK).get
-            Some(new InstitutionCredentials(institution, key))
-          }
+            Some(InstitutionCredentials(institution, key))
         }
       } catch {
-        case t: Throwable => None
+        case _: Throwable => None
       }
     }
-    val list = securityDir.listFiles.filter(f => isCredentialFile(f)).map(f => readCredentialFile(f)).flatten.toList
+    val list = securityDir.listFiles.filter(f => isCredentialFile(f)).flatMap(f => readCredentialFile(f)).toList
     list
   }
 
   /**
-   * A cache of institution credentials.  It is periodically purged to avoid keeping credentials in memory longer than necessary.  The
-   * point (aka: 'necessary') is to avoid having to re-read credentials on every institution reference.
-   */
+    * A cache of institution credentials.  It is periodically purged to avoid keeping credentials in memory longer than necessary.  The
+    * point (aka: 'necessary') is to avoid having to re-read credentials on every institution reference.
+    */
   private val institutionCache = ArrayBuffer[InstitutionCredentials]()
 
-  private def addToCache(institutionCredentials: InstitutionCredentials) = institutionCache.synchronized(institutionCache.append(institutionCredentials))
+  private def addToCache(institutionCredentials: InstitutionCredentials): Unit = institutionCache.synchronized(institutionCache.append(institutionCredentials))
 
   /**
-   * Get the credentials of the given institution.  If they have not yet been created, then create them and
-   * save them to storage.  Also do the intelligent thing with caching.
-   */
+    * Get the credentials of the given institution.  If they have not yet been created, then create them and
+    * save them to storage.  Also do the intelligent thing with caching.
+    */
   private def getInstitutionCredentials(institutionPK: Long): InstitutionCredentials = {
 
     institutionCache.find(ic => ic.institution.institutionPK.get == institutionPK) match {
       case Some(institutionCredentials) => institutionCredentials // if it's in the cache, then we're done
-      case _ => {
+      case _ =>
         val institution = Institution.get(institutionPK).get
         // Get it from the stored list
         readSecurityCredentials.find(ic => ic.institution.institutionPK.get == institutionPK) match {
-          case Some(institutionCredentials) => {
+          case Some(institutionCredentials) =>
             addToCache(institutionCredentials)
             institutionCredentials
-          }
           // if it's not in the stored list, then create it, write it to the stored list, add it to the cache, and return it
-          case _ => {
-            val institutionCredentials = new InstitutionCredentials(institution, Crypto.makeRandomCipherKey)
-            institutionCredentials.persist
+          case _ =>
+            val institutionCredentials = InstitutionCredentials(institution, Crypto.makeRandomCipherKey)
+            institutionCredentials.persist()
             addToCache(institutionCredentials)
             institutionCredentials
-          }
         }
-      }
     }
   }
 
-  def scheduleCacheExpiration: Unit = {
+  def scheduleCacheExpiration(): Unit = {
+
     /**
-     * Constructing this class starts a thread that will run in the future and remove expired credentials.
-     */
+      * Constructing this class starts a thread that will run in the future and remove expired credentials.
+      */
     class ExpireCache extends Runnable {
-      def run = {
+      def run(): Unit = {
         // add a little extra time to ensure that the operation that cached the credential is done
         Thread.sleep(cacheTimeout + 1000)
         try {
@@ -194,31 +191,31 @@ object AnonymizeUtil extends Logging {
           case t: Throwable => logger.warn("Ignoring unexpected error in cache expiration: " + fmtEx(t))
         }
       }
-      (new Thread(this)).start
+      new Thread(this).start()
     }
     new ExpireCache
   }
 
   /**
-   * Get the key of the given institution.
-   */
+    * Get the key of the given institution.
+    */
   def getInstitutionKey(institutionPK: Long): String = getInstitutionCredentials(institutionPK).key
 
   def encryptWithNonce(institutionPK: Long, text: String): String = {
     val institutionCredentials = getInstitutionCredentials(institutionPK).encrypt(text)
-    scheduleCacheExpiration
+    scheduleCacheExpiration()
     institutionCredentials
   }
 
   def decryptWithNonce(institutionPK: Long, text: String): String = {
     val institutionCredentials = getInstitutionCredentials(institutionPK).decrypt(text)
-    scheduleCacheExpiration
+    scheduleCacheExpiration()
     institutionCredentials
   }
 
   /**
-   * Construct an alias from the given prefix and number.
-   */
+    * Construct an alias from the given prefix and number.
+    */
   def aliasify(aliasPrefix: String, number: Long): String = {
     aliasPrefix + "_" + number.toString
   }
@@ -229,54 +226,56 @@ object AnonymizeUtil extends Logging {
   }
 
   /**
-   * Given the list of attributes that need to be anonymized, return a list of DicomAnonymous entries that
-   * address them.  If the entries do not already exist in the database, then create and insert them.
-   */
+    * Given the list of attributes that need to be anonymized, return a list of DicomAnonymous entries that
+    * address them.  If the entries do not already exist in the database, then create and insert them.
+    */
   private val makeDicomAnonymousListSync = ""
-  def makeDicomAnonymousList(institutionPK: Long, attrList: Seq[Attribute]): Seq[DicomAnonymous] = makeDicomAnonymousListSync.synchronized {
-    val institutionKey = getInstitutionKey(institutionPK)
-    val previouslyAnonymized = DicomAnonymous.getAttributes(institutionPK, attrList)
+  def makeDicomAnonymousList(institutionPK: Long, attrList: Seq[Attribute]): Seq[DicomAnonymous] =
+    makeDicomAnonymousListSync.synchronized {
+      val institutionKey = getInstitutionKey(institutionPK)
+      val previouslyAnonymized = DicomAnonymous.getAttributes(institutionPK, attrList)
 
-    def alreadyHasBeenAnonyimized(prev: Seq[DicomAnonymous], attr: Attribute): Boolean = {
-      val attrHash = DicomAnonymous.makeAttributeHash(institutionKey, attr)
-      val attrText = DicomAnonymous.formatAnonAttributeTag(attr.getTag)
-      prev.find(da => da.attributeHash.equals(attrHash) && da.attributeTag.equals(attrText)).isDefined
-    }
-
-    def addIfNeeded(list: Seq[DicomAnonymous], attr: Attribute): Seq[DicomAnonymous] = {
-      if (alreadyHasBeenAnonyimized(list, attr))
-        list
-      else {
-        val da = DicomAnonymous.insert(institutionPK, attr)
-        list :+ da
+      def alreadyHasBeenAnonyimized(prev: Seq[DicomAnonymous], attr: Attribute): Boolean = {
+        val attrHash = DicomAnonymous.makeAttributeHash(institutionKey, attr)
+        val attrText = DicomAnonymous.formatAnonAttributeTag(attr.getTag)
+        prev.exists(da => da.attributeHash.equals(attrHash) && da.attributeTag.equals(attrText))
       }
-    }
 
-    val updated = attrList.foldLeft(previouslyAnonymized)((list, attr) => addIfNeeded(list, attr))
-    updated
-  }
+      def addIfNeeded(list: Seq[DicomAnonymous], attr: Attribute): Seq[DicomAnonymous] = {
+        if (alreadyHasBeenAnonyimized(list, attr))
+          list
+        else {
+          val da = DicomAnonymous.insert(institutionPK, attr)
+          list :+ da
+        }
+      }
+
+      val updated = attrList.foldLeft(previouslyAnonymized)((list, attr) => addIfNeeded(list, attr))
+      updated
+    }
 
   private val anonymizeDicomSync = ""
+
   /**
-   * Anonymize an attribute list.  This must be synchronized to ensure that consistency of anonymized values.
-   */
-  def anonymizeDicom(institutionPK: Long, source: AttributeList): AttributeList = anonymizeDicomSync.synchronized {
-    // TODO: Make a version that can take Seq[AttributeList] to exploit caching recent values.  But hey, first see if this is a bottleneck.
-    val institutionKey = getInstitutionKey(institutionPK)
-    val dest = DicomUtil.clone(source) // do not modify the input
+    * Anonymize an attribute list.  This must be synchronized to ensure that consistency of anonymized values.
+    */
+  def anonymizeDicom(institutionPK: Long, source: AttributeList): AttributeList =
+    anonymizeDicomSync.synchronized {
+      val institutionKey = getInstitutionKey(institutionPK)
+      val dest = DicomUtil.clone(source) // do not modify the input
 
-    val attrList = getListOfAttributesNeedingAnonymization(dest)
-    val updatedDicomAnonymousList = makeDicomAnonymousList(institutionPK, attrList)
+      val attrList = getListOfAttributesNeedingAnonymization(dest)
+      val updatedDicomAnonymousList = makeDicomAnonymousList(institutionPK, attrList)
 
-    def anonymizeAttribute(attr: Attribute): Unit = {
-      val attrHash = DicomAnonymous.makeAttributeHash(institutionKey, attr)
-      val attrText = DicomAnonymous.formatAnonAttributeTag(attr.getTag)
-      val anonValue = updatedDicomAnonymousList.find(da => da.attributeHash.equals(attrHash) && da.attributeTag.equals(attrText)).get.value
-      attr.removeValues
-      attr.addValue(anonValue)
+      def anonymizeAttribute(attr: Attribute): Unit = {
+        val attrHash = DicomAnonymous.makeAttributeHash(institutionKey, attr)
+        val attrText = DicomAnonymous.formatAnonAttributeTag(attr.getTag)
+        val anonValue = updatedDicomAnonymousList.find(da => da.attributeHash.equals(attrHash) && da.attributeTag.equals(attrText)).get.value
+        attr.removeValues()
+        attr.addValue(anonValue)
+      }
+
+      attrList.foreach(a => anonymizeAttribute(a))
+      dest
     }
-
-    attrList.map(a => anonymizeAttribute(a))
-    dest
-  }
 }
