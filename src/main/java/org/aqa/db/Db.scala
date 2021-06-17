85622cff67a0fe0ebeb938ce332c0fa080445658
Jim Irrer <irrer@umich.edu>
2021-06-17 16:12:57 -0400

    Added message if a database call takes an excessively long time.
    Currently set to 5 seconds.  This is a potential diagnostic tool.
    Message also prints out the top few members of the stack.
    
    Also did some code cleanup.  Hesitant to change too much because
    the database interface is a little tricky.

--- src/main/java/org/aqa/db/Db.scala
+++ src/main/java/org/aqa/db/Db.scala
@@ -1,33 +1,31 @@
 package org.aqa.db
 
-import scala.concurrent.duration.DurationInt
+import org.aqa.Config
+import org.aqa.Logging
+import org.aqa.Util
+import org.aqa.db.Db.driver
 import slick.jdbc.meta.MTable
+import slick.sql.FixedSqlAction
+
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{ Success, Failure }
-import com.mchange.v2.c3p0.ComboPooledDataSource
-import scala.xml.Elem
-import org.aqa.run.ProcedureStatus
-import org.aqa.Logging
-import java.io.File
-import org.aqa.Config
-import scala.xml.XML
-import slick.sql.FixedSqlAction
-import edu.umro.ScalaUtil.Trace
-import com.typesafe.config.ConfigFactory
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.FiniteDuration
+import scala.util.Failure
+import scala.util.Success
 
 /** Schema independent database utilities. */
 
 object Db extends Logging {
 
   /** Ensure that the the configuration has been read. */
-  private val configInit = Config.validate
+   Config.validate
 
-  def isSqlServer = Config.SlickDb.getString("db.default.driver").toLowerCase.contains("sqlserver")
+  def isSqlServer: Boolean = Config.SlickDb.getString("db.default.driver").toLowerCase.contains("sqlserver")
 
   /**
-   * Look at the DB configuration to determine which driver to load.
-   */
+    * Look at the DB configuration to determine which driver to load.
+    */
   val driver = {
     val name = Config.SlickDb.getString("db.default.driver")
     logger.info("Using DB drive: " + name)
@@ -36,15 +34,15 @@ object Db extends Logging {
     if (slick.jdbc.SQLServerProfile == null) logger.warn("slick.jdbc.SQLServerProfile is null")
 
     val d = 0 match {
-      case _ if name.toLowerCase.contains("postgres") => slick.driver.PostgresDriver
+      case _ if name.toLowerCase.contains("postgres")  => slick.driver.PostgresDriver
       case _ if name.toLowerCase.contains("sqlserver") => slick.jdbc.SQLServerProfile
-      case _ if name.toLowerCase.contains("oracle") => slick.jdbc.OracleProfile
-      case _ if name.toLowerCase.contains("mysql") => slick.driver.MySQLDriver
-      case _ if name.toLowerCase.contains("h2") => slick.driver.H2Driver
-      case _ if name.toLowerCase.contains("sqlite") => slick.driver.SQLiteDriver
-      case _ if name.toLowerCase.contains("derby") => slick.driver.DerbyDriver
-      case _ if name.toLowerCase.contains("hsqld") => slick.driver.HsqldbDriver
-      case _ => throw new RuntimeException("Unable to recognize database driver: " + name)
+      case _ if name.toLowerCase.contains("oracle")    => slick.jdbc.OracleProfile
+      case _ if name.toLowerCase.contains("mysql")     => slick.driver.MySQLDriver
+      case _ if name.toLowerCase.contains("h2")        => slick.driver.H2Driver
+      case _ if name.toLowerCase.contains("sqlite")    => slick.driver.SQLiteDriver
+      case _ if name.toLowerCase.contains("derby")     => slick.driver.DerbyDriver
+      case _ if name.toLowerCase.contains("hsqld")     => slick.driver.HsqldbDriver
+      case _                                           => throw new RuntimeException("Unable to recognize database driver: " + name)
     }
     logger.info("Using database driver " + d)
     d
@@ -53,10 +51,10 @@ object Db extends Logging {
   import Db.driver.api._
 
   /**
-   * Default timeout for general database overhead operations.  This includes operations like
-   *  CREATE or DROP that should happen quickly, but does not include SELECT or other operations that could take longer.
-   */
-  val TIMEOUT = new DurationInt(60).seconds
+    * Default timeout for general database overhead operations.  This includes operations like
+    *  CREATE or DROP that should happen quickly, but does not include SELECT or other operations that could take longer.
+    */
+  val TIMEOUT: FiniteDuration = new DurationInt(60).seconds
 
   private def initDb = {
     val prefix = "db.default.db"
@@ -67,71 +65,86 @@ object Db extends Logging {
     logger.info("Attempting to connect to " + msg)
 
     // create the database from the config
-    val d = try {
-      // Using a configuration for Microsoft SQL Server does not work, but it does work using a Slick DataSource.
-      val dd = if (isSqlServer) {
-        logger.info("Configured for Microsoft SQL Server")
-        val ds = new com.microsoft.sqlserver.jdbc.SQLServerDataSource
-        def get(tag: String) = Config.SlickDb.getString("db.default.db." + tag)
-        val userText = get("user").replace('/', '\\')
-        ds.setUser(userText)
-        ds.setIntegratedSecurity(get("integratedSecurity").toBoolean)
-        ds.setPassword(get("password"))
-        ds.setPortNumber(get("port").toInt)
-        ds.setURL(get("url"))
-        ds.setDatabaseName(get("databaseName"))
+    val d =
+      try {
+        // Using a configuration for Microsoft SQL Server does not work, but it does work using a Slick DataSource.
+        val dd = if (isSqlServer) {
+          logger.info("Configured for Microsoft SQL Server")
+          val ds = new com.microsoft.sqlserver.jdbc.SQLServerDataSource
+          def get(tag: String) = Config.SlickDb.getString("db.default.db." + tag)
+          val userText = get("user").replace('/', '\\')
+          ds.setUser(userText)
+          ds.setIntegratedSecurity(get("integratedSecurity").toBoolean)
+          ds.setPassword(get("password"))
+          ds.setPortNumber(get("port").toInt)
+          ds.setURL(get("url"))
+          ds.setDatabaseName(get("databaseName"))
 
-        // prove that we can get a connection.  If this fails, then we can not connect to the database and it is better to know right away
-        logger.info("Attempting to open connection to SQL Server database ...")
-        val connection = ds.getConnection
-        logger.info("Was able to open connection to SQL Server database: " + connection)
+          // prove that we can get a connection.  If this fails, then we can not connect to the database and it is better to know right away
+          logger.info("Attempting to open connection to SQL Server database ...")
+          val connection = ds.getConnection
+          logger.info("Was able to open connection to SQL Server database: " + connection)
 
-        val dbDs = Database.forDataSource(ds, None, AsyncExecutor.default())
-        dbDs
-      } else
-        // for all the other databases that play nicely with config.
-        Database.forConfig(prefix, Config.SlickDb)
-      logger.info("Was able to configure " + msg)
-      dd
-    } catch {
-      case t: Throwable => {
-        logger.error("Unable to create database connection: " + fmtEx(t))
-        throw new RuntimeException("Unable to create database connection: " + fmtEx(t))
+          val dbDs = Database.forDataSource(ds, None, AsyncExecutor.default())
+          dbDs
+        } else
+          // for all the other databases that play nicely with config.
+          Database.forConfig(prefix, Config.SlickDb)
+        logger.info("Was able to configure " + msg)
+        dd
+      } catch {
+        case t: Throwable =>
+          logger.error("Unable to create database connection: " + fmtEx(t))
+          throw new RuntimeException("Unable to create database connection: " + fmtEx(t))
       }
-    }
     d
   }
 
   private var dbValue: Database = null
 
-  def db = TIMEOUT.synchronized {
-    if (dbValue == null) dbValue = initDb
-    dbValue
-  }
+  def db: driver.api.Database =
+    TIMEOUT.synchronized {
+      if (dbValue == null) dbValue = initDb
+      dbValue
+    }
 
   private def tableName(table: TableQuery[Table[_]]): String = table.shaped.value.tableName
 
   def run[R](op: DBIOAction[R, NoStream, Nothing]): R = {
+
+    /** A database operation that takes longer than this (in ms) is considered to have taken a very long time. */
+    val veryLongTime_ms = 0 // 5 * 1000
     try {
+      val start = System.currentTimeMillis()
       val dbAction = db.run(op)
+      val elapsed = System.currentTimeMillis() - start
+      if (elapsed > veryLongTime_ms) {
+        val stackText = Thread.currentThread
+          .getStackTrace()
+          .tail
+          .map(_.toString)
+          .filterNot(_.contains("scala.collection")) // filter out stuff we don't care about
+          .filterNot(_.contains("org.restlet")) // filter out stuff we don't care about
+          .take(10)
+          .map(se => "    " + se)
+          .mkString("\n")
+        logger.info("Database operation took the very long time of " + Util.elapsedTimeHumanFriendly(elapsed) + " when called from\n" + stackText)
+      }
       val result = Await.result(dbAction, TIMEOUT)
       dbAction.onComplete {
-        case Failure(ex) => {
+        case Failure(ex) =>
           val stackTrace = fmtEx(new RuntimeException("Db.run stack trace"))
           logger.warn("Error from database: " + fmtEx(ex) + "\nAQA source stack trace:" + stackTrace)
-          throw (ex)
-        }
-        case Success(data) => {
-        }
+          throw ex
+        case Success(_) =>
       }
       result
     } catch {
-      case ex: Throwable => {
+      case ex: Throwable =>
         val stackTrace = fmtEx(new RuntimeException("Db.run stack trace from Slick internal error"))
         val msg = "Error from Slick: " + fmtEx(ex) + "\nAQA source stack trace:" + stackTrace
         logger.warn(msg)
         throw new RuntimeException(msg)
-      }
 
     }
 
@@ -142,20 +155,21 @@ object Db extends Logging {
     run(DBIO.seq(dbOperation))
   }
 
-  def perform(ops: Seq[FixedSqlAction[Int, NoStream, Effect.Write]]) = {
+  def perform(ops: Seq[FixedSqlAction[Int, NoStream, Effect.Write]]): Seq[Int] = {
     run(DBIO.sequence(ops))
   }
 
   private object TableList {
+
     /** List of database tables in lower case. Do not reference this directly, instead use <code>getTableList</code> .*/
     private var tableList: Seq[String] = null
 
-    def resetTableList = tableList = null
+    def resetTableList(): Unit = tableList = null
 
     /**
-     * Get the latest version of the list of tables.
-     */
-    def getTableList = {
+      * Get the latest version of the list of tables.
+      */
+    def getTableList: Seq[String] = {
       if (tableList == null) {
         val tableListMixed = if (isSqlServer) {
           //val action = sql"select TABLE_NAME from AQAmsDV.INFORMATION_SCHEMA.TABLES".as[String]
@@ -171,8 +185,8 @@ object Db extends Logging {
     }
 
     /**
-     * Determine if table exists in database
-     */
+      * Determine if table exists in database
+      */
     def tableExists(table: TableQuery[Table[_]]): Boolean = {
       val exists = TableList.getTableList.contains(tableName(table).toLowerCase)
       exists
@@ -181,8 +195,8 @@ object Db extends Logging {
   }
 
   /**
-   * If the given table exists, drop it.
-   */
+    * If the given table exists, drop it.
+    */
   def dropTableIfExists(table: TableQuery[Table[_]]): Unit = {
     if (TableList.tableExists(table)) {
       perform(table.schema.drop)
