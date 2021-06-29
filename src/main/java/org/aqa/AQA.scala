db37cc1555ca60f8291e453dd233145e8f320ef5
Jim Irrer <irrer@umich.edu>
2021-06-29 18:31:14 -0400

    Added Apache 2 license to scala, java, and HTML files.
    It reads:
    
         Copyright 2021 Regents of the University of Michigan
    
         Licensed under the Apache License, Version 2.0 (the "License");
         you may not use this file except in compliance with the License.
         You may obtain a copy of the License at
    
             http://www.apache.org/licenses/LICENSE-2.0
    
         Unless required by applicable law or agreed to in writing, software
         distributed under the License is distributed on an "AS IS" BASIS,
         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
         See the License for the specific language governing permissions and
         limitations under the License.

--- src/main/java/org/aqa/AQA.scala
+++ src/main/java/org/aqa/AQA.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2021 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package org.aqa
 
 import edu.umro.ScalaUtil.PeriodicRestart
@@ -8,10 +25,11 @@ import org.aqa.web.WebServer
 import java.util.Date
 
 /**
- * Main service entry point.  Start up major portions of
- * the service and catch unexpected errors.
- */
+  * Main service entry point.  Start up major portions of
+  * the service and catch unexpected errors.
+  */
 object AQA extends Logging {
+
   /** Time at which service was started. */
   val serviceStartTime: Long = System.currentTimeMillis
 
