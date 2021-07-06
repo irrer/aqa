5b8b13045004dd87fcfc3776f65dc03f8f4a98eb
Jim Irrer <irrer@umich.edu>
2021-07-06 11:31:14 -0400

    Updated C3 and D3 css and js.

--- src/main/java/org/aqa/web/WebUtil.scala
+++ src/main/java/org/aqa/web/WebUtil.scala
@@ -469,9 +469,9 @@ object WebUtil extends Logging {
       if (c3) {
         // There are newer versions of these files but they don't seem to work in AQA.
         Seq(
-          <link href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css" rel="stylesheet"/>,
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js"></script>,
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js"></script>)
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.css" rel="stylesheet"/>,
+          <script src="https://c3js.org/js/d3-5.8.2.min-c5268e33.js" type="text/javascript"></script>,
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.js"></script>)
       } else Seq[Elem]()
     }
 
