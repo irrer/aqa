d67705f2005d3d087d5eb5d4a6ac87f9362875c8
Jim Irrer <irrer@umich.edu>
2021-08-10 11:49:40 -0400

    Simple program to fetch specific item from database.
    This is not to be used in production.

--- src/test/java/learn/SimpleDb.scala
+++ src/test/java/learn/SimpleDb.scala
@@ -16,46 +16,33 @@
 
 package learn
 
-import com.typesafe.config.ConfigFactory
+import edu.umro.ScalaUtil.DicomUtil
 import edu.umro.ScalaUtil.Trace
+import org.aqa.Util
+import org.aqa.db.DbSetup
+import org.aqa.db.DicomSeries
+
 import java.io.File
-import slick.jdbc.JdbcBackend.DatabaseDef
-import slick.basic.DatabaseConfig
-import slick.jdbc.JdbcProfile
 
-//object SimpleDb {
-//  def main(args: Array[String]): Unit = {
-//    Trace.trace("Starting")
-//    val file = new File("""D:\pf\eclipse\workspaceOxygen\aqa\src\test\resources\dbConfig\simpleDb.conf""")
-//    val config = ConfigFactory.parseFile(file)
-//    //val foo: String = config.getString("simple-lib.foo")
-//    //Trace.trace("foo: " + foo)
-//    //val stuff = com.typesafe.config.ConfigFactory.load(config)
-//    Trace.trace(config.toString.split(",").mkString("\n"))
-//
-//    trait SmplDb {
-//      val config: DatabaseConfig[JdbcProfile]
-//      val db: JdbcProfile#Backend#Database = config.db
-//
-//      case class User(id: Option[Int], email: String,
-//        firstName: Option[String], lastName: Option[String])
-//
-//      case class Address(id: Option[Int], userId: Int,
-//        addressLine: String, city: String, postalCode: String)
-//
-//     // def find2(id: Int) = db.run(User.filter(_.id === id).result.headOption)
-//    }
-//    
-//    class Thing extends SmplDb {
-//     // def find(id: Int) = db.run(users.filter(_.id === id).result.headOption)
-//
-//    }
-//
-//    val patId = "FooFoo"
-//
-//    // https://scala-slick.org/doc/3.2.0/api/slick/jdbc/JdbcBackend.html#Database=JdbcBackend.this.DatabaseDef
-//    Trace.trace("Done")
-//  }
-//}
+object SimpleDb {
+  def main(args: Array[String]): Unit = {
+    Trace.trace("Starting")
+    DbSetup.init
 
+    val ds = DicomSeries.getBySeriesInstanceUID("1.3.6.1.4.1.22361.7091168952785.634753698.1569527528282.57")
+    if (ds.isEmpty)
+      Trace.trace("Could not find DICOM series")
+    else {
+      val alList = ds.head.attributeListList
+      println("Number of attribute lists: " + alList.size)
+      val outDir = new File("""D:\tmp\aqa\tmp\ds""")
+      alList.foreach(al => {
+        val file = new File(outDir, Util.sopOfAl(al) + ".dcm")
+        DicomUtil.writeAttributeListToFile(al, file, "SimpleDb")
+        Trace.trace("wrote DICOM file " + file.getAbsolutePath)
+      })
+    }
 
+    Trace.trace("Done")
+  }
+}
