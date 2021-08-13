254bd867b86d213163b84297117dddaf57a2271b
Jim Irrer <irrer@umich.edu>
2021-08-13 11:15:26 -0400

    Code cleanup.

--- src/main/java/org/aqa/webrun/phase2/collimatorCentering/CollimatorCenteringHTML.scala
+++ src/main/java/org/aqa/webrun/phase2/collimatorCentering/CollimatorCenteringHTML.scala
@@ -16,22 +16,21 @@
 
 package org.aqa.webrun.phase2.collimatorCentering
 
-import org.aqa.db.Output
-import scala.xml.Elem
-import java.awt.image.BufferedImage
-import org.aqa.Config
+import edu.umro.ImageUtil.IsoImagePlaneTranslator
 import edu.umro.ScalaUtil.FileUtil
-import java.io.File
-import javax.imageio.ImageIO
-import org.aqa.run.ProcedureStatus
+import org.aqa.Config
 import org.aqa.Util
 import org.aqa.db.CollimatorCentering
-import java.awt.geom.Point2D
+import org.aqa.run.ProcedureStatus
 import org.aqa.webrun.ExtendedData
 import org.aqa.webrun.phase2.MeasureTBLREdges
 import org.aqa.webrun.phase2.Phase2Util
 import org.aqa.webrun.phase2.RunReq
-import edu.umro.ImageUtil.IsoImagePlaneTranslator
+
+import java.awt.geom.Point2D
+import java.awt.image.BufferedImage
+import java.io.File
+import scala.xml.Elem
 
 object CollimatorCenteringHTML {
 
@@ -44,9 +43,9 @@ object CollimatorCenteringHTML {
     val pngFile = new File(outputDir, fn)
     Util.writePng(bufImg, pngFile)
     <div>
-      <center id={ id }>
-        <a href={ fn }>
-          <img src={ fn } class="img-responsive"/>
+      <center id={id}>
+        <a href={fn}>
+          <img src={fn} class="img-responsive"/>
         </a>
       </center>
     </div>
@@ -57,21 +56,22 @@ object CollimatorCenteringHTML {
     case class CCRow(title: String, value: Double)
 
     val rowList = Seq(
-      new CCRow("X collimator center", cc.xCollimatorCenter_mm),
-      new CCRow("Y collimator center", cc.yCollimatorCenter_mm),
-      new CCRow("X1 90 degrees", cc.X1_090_mm),
-      new CCRow("X2 90 degrees", cc.X2_090_mm),
-      new CCRow("Y1 90 degrees", cc.Y1_090_mm),
-      new CCRow("Y2 90 degrees", cc.Y2_090_mm),
-      new CCRow("X1 270 degrees", cc.X1_270_mm),
-      new CCRow("X2 270 degrees", cc.X2_270_mm),
-      new CCRow("Y1 270 degrees", cc.Y1_270_mm),
-      new CCRow("Y2 270 degrees", cc.Y2_270_mm))
+      CCRow("X collimator center", cc.xCollimatorCenter_mm),
+      CCRow("Y collimator center", cc.yCollimatorCenter_mm),
+      CCRow("X1 90 degrees", cc.X1_090_mm),
+      CCRow("X2 90 degrees", cc.X2_090_mm),
+      CCRow("Y1 90 degrees", cc.Y1_090_mm),
+      CCRow("Y2 90 degrees", cc.Y2_090_mm),
+      CCRow("X1 270 degrees", cc.X1_270_mm),
+      CCRow("X2 270 degrees", cc.X2_270_mm),
+      CCRow("Y1 270 degrees", cc.Y1_270_mm),
+      CCRow("Y2 270 degrees", cc.Y2_270_mm)
+    )
 
     def rowToHtml(ccRow: CCRow): Elem = {
       <tr>
-        <td>{ ccRow.title }</td>
-        <td>{ fmt(ccRow.value) }</td>
+        <td>{ccRow.title}</td>
+        <td>{fmt(ccRow.value)}</td>
       </tr>
     }
 
@@ -81,30 +81,37 @@ object CollimatorCenteringHTML {
           <th>Description</th>
           <th>Value</th>
         </tr>
-        { rowList.map(row => rowToHtml(row)) }
+        {rowList.map(row => rowToHtml(row))}
       </table>
     }
     table
   }
 
   /**
-   * Make a tiny summary and link to the detailed report.
-   */
+    * Make a tiny summary and link to the detailed report.
+    */
   private def makeSummary(status: ProcedureStatus.Value, resultSummary: String) = {
     val iconImage = if (status == ProcedureStatus.pass) Config.passImageUrl else Config.failImageUrl
     val elem = {
       <div title="Click for details.">
-        <a href={ htmlFileName }>
-          { CollimatorCenteringAnalysis.subProcedureName }<br/>
-          { resultSummary }<br/>
-          <img src={ iconImage } height="32"/>
+        <a href={htmlFileName}>
+          {CollimatorCenteringAnalysis.subProcedureName}<br/>
+          {resultSummary}<br/>
+          <img src={iconImage} height="32"/>
         </a>
       </div>
     }
     elem
   }
 
-  def makeDisplay(extendedData: ExtendedData, collimatorCentering: CollimatorCentering, status: ProcedureStatus.Value, image090: MeasureTBLREdges.AnalysisResult, image270: MeasureTBLREdges.AnalysisResult, runReq: RunReq): Elem = {
+  def makeDisplay(
+      extendedData: ExtendedData,
+      collimatorCentering: CollimatorCentering,
+      status: ProcedureStatus.Value,
+      image090: MeasureTBLREdges.AnalysisResult,
+      image270: MeasureTBLREdges.AnalysisResult,
+      runReq: RunReq
+  ): Elem = {
 
     val attr090 = runReq.rtimageMap(Config.CollimatorCentering090BeamName)
     val attr270 = runReq.rtimageMap(Config.CollimatorCentering270BeamName)
@@ -122,7 +129,7 @@ object CollimatorCenteringHTML {
     def imageTitle(collAngle: Int, isoCenter: Point2D.Double): Elem = {
       val isoCenterText = fmt(isoCenter.getX) + ", " + fmt(isoCenter.getY)
       val title = "Collimator Angle " + collAngle + " : image center in isoplane " + isoCenterText
-      <h3 title={ title } style="text-align:center;">{ collAngle.toString + " : " + isoCenterText }</h3>
+      <h3 title={title} style="text-align:center;">{collAngle.toString + " : " + isoCenterText}</h3>
     }
 
     val image090Name = "image090"
@@ -143,22 +150,22 @@ object CollimatorCenteringHTML {
 
       <div>
         <div class="col-md-4 col-md-offset-3" align="middle">
-          <h3 title='X, Y difference from isoplane center in mm'>{ resultSummary } mm</h3>
+          <h3 title='X, Y difference from isoplane center in mm'>{resultSummary} mm</h3>
         </div>
         <div class="row" style="margin:30px;">
           <div class="col-md-4 col-md-offset-1" align="middle">
-            { imageTitle(90, isoCenter090) }
-            <a title='Click for DICOM details' href={ href090 }>{ Config.CollimatorCentering090BeamName }<br/></a>
-            { showImage("CollimatorCentering090_" + Config.CollimatorCentering090BeamName + ".png", outputDir, image090.bufferedImage, image090Name) }
+            {imageTitle(90, isoCenter090)}
+            <a title='Click for DICOM details' href={href090}>{Config.CollimatorCentering090BeamName}<br/></a>
+            {showImage("CollimatorCentering090_" + Config.CollimatorCentering090BeamName + ".png", outputDir, image090.bufferedImage, image090Name)}
           </div>
           <div class="col-md-4" align="middle">
-            { imageTitle(270, isoCenter270) }
-            <a title='Click for DICOM details' href={ href270 }>{ Config.CollimatorCentering270BeamName }<br/></a>
-            { showImage("CollimatorCentering270_" + Config.CollimatorCentering270BeamName + ".png", outputDir, image270.bufferedImage, image270Name) }
+            {imageTitle(270, isoCenter270)}
+            <a title='Click for DICOM details' href={href270}>{Config.CollimatorCentering270BeamName}<br/></a>
+            {showImage("CollimatorCentering270_" + Config.CollimatorCentering270BeamName + ".png", outputDir, image270.bufferedImage, image270Name)}
           </div>
         </div>
         <div class="col-md-4 col-md-offset-4" align="middle">
-          { makeTable(collimatorCentering) }
+          {makeTable(collimatorCentering)}
         </div>
       </div>
     }
