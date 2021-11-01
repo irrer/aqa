3d43ed72c068c3c7ee92f23d0541f6d408f5fa4a
Jim Irrer <irrer@umich.edu>
2021-11-01 15:24:38 -0400

    Cleaned up code.

--- src/main/java/org/aqa/web/MaintenanceRecordUpdate.scala
+++ src/main/java/org/aqa/web/MaintenanceRecordUpdate.scala
@@ -16,27 +16,18 @@
 
 package org.aqa.web
 
-import org.restlet.Restlet
+import org.aqa.Config
+import org.aqa.db.Machine
+import org.aqa.db.MaintenanceRecord
+import org.aqa.web.WebUtil._
 import org.restlet.Request
 import org.restlet.Response
-import org.restlet.data.Method
+import org.restlet.Restlet
+import org.restlet.data.Status
+
+import java.sql.Timestamp
 import java.util.Date
 import scala.xml.Elem
-import org.restlet.data.Parameter
-import org.aqa.db.MaintenanceRecord
-import scala.concurrent.ExecutionContext.Implicits.global
-import play.api._
-import play.api.libs.concurrent.Execution.Implicits._
-import org.restlet.data.Form
-import scala.xml.PrettyPrinter
-import org.restlet.data.Status
-import org.restlet.data.MediaType
-import WebUtil._
-import scala.concurrent.Await
-import scala.concurrent.duration.DurationInt
-import org.aqa.db.Machine
-import java.sql.Timestamp
-import org.aqa.Config
 
 object MaintenanceRecordUpdate {
   val maintenanceRecordPKTag = "maintenanceRecordPK"
@@ -56,24 +47,22 @@ class MaintenanceRecordUpdate extends Restlet with SubUrlAdmin {
 
   private def getOutputReference(valueMap: ValueMapT): Elem = {
     try {
-      val pk = valueMap.get(MaintenanceRecordUpdate.maintenanceRecordPKTag).get.toLong
+      val pk = valueMap(MaintenanceRecordUpdate.maintenanceRecordPKTag).toLong
       val outputPK = MaintenanceRecord.get(pk).get.outputPK.get
       val url = "/view/ViewOutput?outputPK=" + outputPK
-      <a href={ url }>View Originating Output</a>
+      <a href={url}>View Originating Output</a>
     } catch {
-      case t: Throwable => {
-        <div>No related output</div>
-      }
+      case _: Throwable => <div>No related output</div>
     }
   }
 
-  private val outputReference = new WebPlainText("RelatedOutput", false, 6, 0, getOutputReference _)
+  private val outputReference = new WebPlainText("RelatedOutput", false, 6, 0, getOutputReference)
 
   //private val user = new WebInputText("User", 6, 0, "")
 
   private val summary = new WebInputText("Summary", 6, 0, "")
 
-  private val category = new WebInputSelect("Category", 6, 0, (response: Option[Response]) => Config.MaintenanceCategoryList.map(m => (m.Name, m.Name)))
+  private val category = new WebInputSelect("Category", 6, 0, (_: Option[Response]) => Config.MaintenanceCategoryList.map(m => (m.Name, m.Name)))
 
   private val description = new WebInputTextArea("Description", 6, 0, "")
 
@@ -82,17 +71,12 @@ class MaintenanceRecordUpdate extends Restlet with SubUrlAdmin {
     new FormButton(name, 1, 0, subUrl, action, buttonType)
   }
 
-  private val createButton = makeButton("Create", true, ButtonType.BtnPrimary)
-  private val saveButton = makeButton("Save", true, ButtonType.BtnPrimary)
-  private val deleteButton = makeButton("Delete", false, ButtonType.BtnDanger)
-  private val cancelButton = makeButton("Cancel", false, ButtonType.BtnDefault)
+  private val createButton = makeButton("Create", primary = true, ButtonType.BtnPrimary)
+  private val saveButton = makeButton("Save", primary = true, ButtonType.BtnPrimary)
+  private val deleteButton = makeButton("Delete", primary = false, ButtonType.BtnDanger)
+  private val cancelButton = makeButton("Cancel", primary = false, ButtonType.BtnDefault)
 
-  val fieldList: List[WebRow] = List(
-    List(summary),
-    List(category),
-    List(description),
-    List(outputReference),
-    List(dateTime))
+  val fieldList: List[WebRow] = List(List(summary), List(category), List(description), List(outputReference), List(dateTime))
 
   val createButtonList: WebRow = List(createButton, cancelButton, machinePK)
   val editButtonList: WebRow = List(saveButton, cancelButton, deleteButton, machinePK, maintenanceRecordPK)
@@ -107,24 +91,23 @@ class MaintenanceRecordUpdate extends Restlet with SubUrlAdmin {
   }
 
   /**
-   * Make sure that the user is authorized.  They must be from the same institution as the machine.
-   *
-   * Allowing a whitelisted user is probably not a good idea because the ue
-   */
+    * Make sure that the user is authorized.  They must be from the same institution as the machine.
+    *
+    * Allowing a whitelisted user is probably not a good idea because the ue
+    */
   private def isAuthorized(request: Request, institutionPK: Long): Boolean = {
-    val ok =
-      {
-        getUser(request) match {
-          case Some(user) => user.institutionPK == institutionPK
-          case _ => false
-        }
+    val ok = {
+      getUser(request) match {
+        case Some(user) => user.institutionPK == institutionPK
+        case _          => false
       }
+    }
     ok
   }
 
   private def emptySummary(valueMap: ValueMapT): StyleMapT = {
-    val summaryText = valueMap.get(summary.label).get.trim
-    val isEmpty = summaryText.trim.size == 0
+    val summaryText = valueMap(summary.label).trim
+    val isEmpty = summaryText.trim.isEmpty
     if (isEmpty) Error.make(summary, summary.label + " can not be empty")
     else styleNone
   }
@@ -132,10 +115,10 @@ class MaintenanceRecordUpdate extends Restlet with SubUrlAdmin {
   private def validateAuthentication(valueMap: ValueMapT, request: Request): StyleMapT = {
 
     val machPK = {
-      if (valueMap.get(machinePK.label).isDefined) {
-        valueMap.get(machinePK.label).get.toLong
+      if (valueMap.contains(machinePK.label)) {
+        valueMap(machinePK.label).toLong
       } else {
-        (MaintenanceRecord.get(valueMap.get(maintenanceRecordPK.label).get.toLong).get).machinePK
+        MaintenanceRecord.get(valueMap(maintenanceRecordPK.label).toLong).get.machinePK
       }
     }
     val instPK = Machine.get(machPK).get.institutionPK
@@ -147,8 +130,8 @@ class MaintenanceRecordUpdate extends Restlet with SubUrlAdmin {
   private def checkFields(valueMap: ValueMapT, request: Request): StyleMapT = validateAuthentication(valueMap, request) ++ emptySummary(valueMap)
 
   /**
-   * Create a new maintenanceRecord
-   */
+    * Create a new maintenanceRecord
+    */
   private def createMaintenanceRecordFromParameters(valueMap: ValueMapT, request: Request): MaintenanceRecord = {
     val mrPK: Option[Long] = {
       val e = valueMap.get(MaintenanceRecordUpdate.maintenanceRecordPKTag)
@@ -158,41 +141,40 @@ class MaintenanceRecordUpdate extends Restlet with SubUrlAdmin {
     val machPK = machinePK.getValOrEmpty(valueMap).trim.toLong
     val dt = new Timestamp(dateTime.dateFormat.parse(dateTime.getValOrEmpty(valueMap)).getTime)
     val uPK = getUser(request).get.userPK.get
-    val cat = valueMap.get(category.label).get
+    val cat = valueMap(category.label)
 
     new MaintenanceRecord(
-      maintenanceRecordPK =  mrPK,
-      category =  cat,
+      maintenanceRecordPK = mrPK,
+      category = cat,
       machinePK = machPK,
-      creationTime =  dt,
-      userPK =  uPK,
-      outputPK =  None, // outputPK
+      creationTime = dt,
+      userPK = uPK,
+      outputPK = None, // outputPK
       machineLogPK = None,
       machineLogNodeIndex = None,
       summary.getValOrEmpty(valueMap).trim,
-      description.getValOrEmpty(valueMap).trim)
+      description.getValOrEmpty(valueMap).trim
+    )
 
   }
 
-  private def emptyForm(response: Response, valueMap: ValueMapT) = {
-    val vm = Map(
-      (machinePK.label, valueMap(machinePK.label)),
-      (dateTime.label, dateTime.dateFormat.format(new Date)))
+  private def emptyForm(response: Response, valueMap: ValueMapT): Unit = {
+    val vm = Map((machinePK.label, valueMap(machinePK.label)), (dateTime.label, dateTime.dateFormat.format(new Date)))
     formCreate.setFormResponse(vm, styleNone, pageTitleCreate, response, Status.SUCCESS_OK)
   }
 
   /**
-   * Save changes made to form.
-   */
-  private def save(valueMap: ValueMapT, pageTitle: String, response: Response): Unit = {
+    * Save changes made to form.
+    */
+  private def save(valueMap: ValueMapT, response: Response): Unit = {
     val styleMap = checkFields(valueMap, response.getRequest)
     if (styleMap.isEmpty) {
-      (createMaintenanceRecordFromParameters(valueMap, response.getRequest)).insertOrUpdate
+      createMaintenanceRecordFromParameters(valueMap, response.getRequest).insertOrUpdate()
       redirectToList(response, valueMap)
     } else formEdit.setFormResponse(valueMap, styleMap, pageTitleEdit, response, Status.CLIENT_ERROR_BAD_REQUEST)
   }
 
-  private def create(valueMap: ValueMapT, pageTitle: String, response: Response) = {
+  private def create(valueMap: ValueMapT, response: Response): Unit = {
     val styleMap = checkFields(valueMap, response.getRequest)
     if (styleMap.isEmpty) {
       createMaintenanceRecordFromParameters(valueMap, response.getRequest).insert
@@ -200,14 +182,15 @@ class MaintenanceRecordUpdate extends Restlet with SubUrlAdmin {
     } else formCreate.setFormResponse(valueMap, styleMap, pageTitleCreate, response, Status.CLIENT_ERROR_BAD_REQUEST)
   }
 
-  private def edit(inst: MaintenanceRecord, response: Response) = {
+  private def edit(inst: MaintenanceRecord, response: Response): Unit = {
     val valueMap = Map(
       (maintenanceRecordPK.label, inst.maintenanceRecordPK.get.toString),
       (category.label, inst.category),
       (machinePK.label, inst.machinePK.toString),
       (dateTime.label, dateTime.dateFormat.format(inst.creationTime)),
       (summary.label, inst.summary),
-      (description.label, inst.description))
+      (description.label, inst.description)
+    )
     formEdit.setFormResponse(valueMap, styleNone, pageTitleEdit, response, Status.SUCCESS_OK)
   }
 
@@ -248,15 +231,14 @@ class MaintenanceRecordUpdate extends Restlet with SubUrlAdmin {
 
   private def buttonIs(valueMap: ValueMapT, button: FormButton): Boolean = {
     val value = valueMap.get(button.label)
-    value.isDefined && value.get.toString.equals(button.label)
+    value.isDefined && value.get.equals(button.label)
   }
 
   /**
-   * Determine if the incoming request is to edit an existing MaintenanceRecord.
-   */
+    * Determine if the incoming request is to edit an existing MaintenanceRecord.
+    */
   private def isEdit(valueMap: ValueMapT, response: Response): Boolean = {
     val inst = getReference(valueMap)
-    val value = valueMap.get(MaintenanceRecordUpdate.maintenanceRecordPKTag)
     if (inst.isDefined) {
       edit(inst.get, response)
       true
@@ -270,16 +252,14 @@ class MaintenanceRecordUpdate extends Restlet with SubUrlAdmin {
     try {
       0 match {
         case _ if buttonIs(valueMap, cancelButton) => redirectToList(response, valueMap)
-        case _ if buttonIs(valueMap, createButton) => create(valueMap, pageTitleEdit, response)
-        case _ if buttonIs(valueMap, saveButton) => save(valueMap, pageTitleEdit, response)
-        case _ if isDelete(valueMap, response) => Nil
-        case _ if isEdit(valueMap, response) => Nil
-        case _ => emptyForm(response, valueMap)
+        case _ if buttonIs(valueMap, createButton) => create(valueMap, response)
+        case _ if buttonIs(valueMap, saveButton)   => save(valueMap, response)
+        case _ if isDelete(valueMap, response)     =>
+        case _ if isEdit(valueMap, response)       =>
+        case _                                     => emptyForm(response, valueMap)
       }
     } catch {
-      case t: Throwable => {
-        WebUtil.internalFailure(response, t)
-      }
+      case t: Throwable => WebUtil.internalFailure(response, t)
     }
   }
 }
