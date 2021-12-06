ba36528c7cdb66130dbad559422bcd4a51e54474
Jim Irrer <irrer@umich.edu>
2021-12-06 09:30:51 -0500

    Fixed compiler warning.

--- src/main/java/org/aqa/stats/SymFlatCu.scala
+++ src/main/java/org/aqa/stats/SymFlatCu.scala
@@ -5,9 +5,8 @@ import org.aqa.db.Procedure
 import org.aqa.db.SymmetryAndFlatness
 
 abstract class SymFlatCu extends MachineValue {
-  def get(machinePK: Long, cuOf: (SymmetryAndFlatness) => Double) = {
+  def get(machinePK: Long, cuOf: (SymmetryAndFlatness) => Double): Unit = {
     val outList = Output.getByMachineAndProcedure(machinePK, Procedure.ProcOfPhase2.get.procedurePK.get)
-
-
+    ???
   }
 }
