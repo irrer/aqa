d586b4899339086477fdc40abc520f3919ed0079
Jim Irrer <irrer@umich.edu>
2022-02-22 07:08:47 -0500

    Move learning code to appropriate directory.

--- src/test/java/learn/Band.scala
+++ src/test/java/learn/Band.scala
@@ -1,12 +1,18 @@
-package org.aqa.webrun.gapSkew
+package learn
 
 import edu.umro.ImageUtil.DicomImage
+import edu.umro.ImageUtil.ImageUtil
+import edu.umro.ImageUtil.IsoImagePlaneTranslator
 import org.aqa.DicomFile
 import org.aqa.Util
 
+import java.awt.Color
 import java.awt.Rectangle
 import java.io.File
 
+/**
+ * View image characteristics.  Review coordinate system.
+ */
 object Band {
   def main(args: Array[String]): Unit = {
     val file = new File("""D:\tmp\aqa\GapSkew\dicom\Study_1\RTIMAGE_01\RTIMAGE_002_2020-03-23T19-13-17.000.dcm""")
@@ -14,8 +20,8 @@ object Band {
     val al = df.attributeList.get
     val di = new DicomImage(al)
 
-    val top = 360
-    val height = 5
+    val top = 0
+    val height = di.height
     val rect = new Rectangle(0, top, di.width, height)
     val sub = di.getSubimage(rect)
     val sums = {
@@ -27,6 +33,25 @@ object Band {
     println(sums.mkString("\n", "\n", "\n"))
 
     val img = sub.toDeepColorBufferedImage(0.01)
+    if (true) {
+      val trans = new IsoImagePlaneTranslator(al)
+      val g = ImageUtil.getGraphics(img)
+      val dim = 10
+
+      def draw(pos: Int) = {
+        g.setColor(Color.white)
+        val p1 = trans.iso2Pix(pos, pos)
+        println(p1)
+        g.drawRect(Util.d2i(p1.getX), Util.d2i(p1.getY), dim, dim)
+        g.drawString(pos.toString, Util.d2i(p1.getX), Util.d2i(p1.getY))
+      }
+
+      draw(-100)
+      draw(-50)
+      draw(0)
+      draw(50)
+      draw(100)
+    }
     val unique = (System.currentTimeMillis() % 1000).formatted("%3d")
     val imgFile = new File(file.getParentFile, file.getName.dropRight(4) + unique + ".png")
     Util.writePng(img, imgFile)
