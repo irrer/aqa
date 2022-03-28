70297e1f8824c7f1788eca170c4c49c2f17056f1
Jim Irrer <irrer@umich.edu>
2022-03-28 18:14:32 -0400

    Added convenience link back to main report.

--- src/main/java/org/aqa/webrun/gapSkew/GapSkewDetailHtml.scala
+++ src/main/java/org/aqa/webrun/gapSkew/GapSkewDetailHtml.scala
@@ -198,7 +198,12 @@ case class GapSkewDetailHtml(extendedData: ExtendedData, leafSet: LeafSet, runRe
       <div class="col-md-12 col-md-offset-1">
         <div class="row">
           <div class="col-md-4 col-md-offset-4">
-            <center> {leafTitle} </center>
+            <center>
+              {leafTitle}
+              <br/>
+              <a href="display.html">Back to main report</a>
+              <p></p>
+            </center>
           </div>
         </div>
 
