690341ab23771c77712782f686cc8c4051bedb4b
Jim Irrer <irrer@umich.edu>
2022-11-07 11:15:51 -0500

    Added comment clarifying the processing of duplicate beam references.

--- src/main/java/org/aqa/webrun/gapSkew/GapSkewRun.scala
+++ src/main/java/org/aqa/webrun/gapSkew/GapSkewRun.scala
@@ -46,6 +46,15 @@ import scala.annotation.tailrec
 import scala.xml.Elem
 
 class GapSkewRun(procedure: Procedure) extends WebRunProcedure(procedure) with RunTrait[GapSkewRunReq] {
+
+  /**
+   * Get the map of images to be used.  If there is more than one image that reference the same beam number, then
+   * use the latest (chronologically) one.  Choosing one image for each beam addresses the problem of ambiguity.
+   * Choosing the latest one allows the operator to fix mistakes (re-capture images).
+   * @param rtplan
+   * @param rtimageList
+   * @return
+   */
   private def makeRtimageMap(rtplan: AttributeList, rtimageList: Seq[AttributeList]): Map[String, AttributeList] = {
     // sort chronologically
     val sortedRtimageList = rtimageList.sortBy(r => Util.extractDateTimeAndPatientIdFromDicomAl(r)._1.head.getTime)
