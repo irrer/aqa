de1c64cc7a52581341a9a02109b76caef81de646
Jim Irrer <irrer@umich.edu>
2023-12-13 20:29:07 -0500

    More work on PSM prototype. Adding handling of pixels with zero values.

--- src/test/java/learn/psm/PSM.scala
+++ src/test/java/learn/psm/PSM.scala
@@ -10,8 +10,6 @@ import edu.umro.ScalaUtil.FileUtil
 import edu.umro.ScalaUtil.Trace
 import org.aqa.DicomFile
 import org.aqa.Util
-import org.aqa.web.C3Chart
-import org.aqa.webrun.phase2.wedge.WedgeHTML
 import us.hebi.matlab.mat.format.Mat5
 
 import java.awt.Rectangle
@@ -26,14 +24,19 @@ object PSM {
   private def middleOf(image: DicomImage): String = {
 
     val size = 10
-    val x = (image.width - size) / 2
-    val y = (image.height - size) / 2
+    // val x = (image.width - size) / 2
+    // val y = (image.height - size) / 2
+
+    val x = 0
+    val y = 0
 
     val rect = new Rectangle(x, y, size, size)
 
     val sub = image.getSubimage(rect)
 
-    val minMaxMean = s"min: ${image.minPixelValue}    max: ${image.maxPixelValue}    mean: ${image.sum / (image.width * image.height)}"
+    val distinctCount = image.pixelData.flatten.groupBy(p => p).size
+
+    val minMaxMean = s"min: ${image.minPixelValue}    max: ${image.maxPixelValue}    mean: ${image.sum / (image.width * image.height)}    number of distinct values: $distinctCount"
 
     "\n" + minMaxMean + sub.pixelsToText
   }
@@ -68,9 +71,11 @@ object PSM {
 
     def fix(y: Int): IndexedSeq[Float] = {
       (0 until width).map(x => {
-        if (pixelData(y)(x) <= 0)
+        if (pixelData(y)(x) <= 0) {
+          val l = pixelData(y - 1)(x)
+          val h = pixelData(y + 1)(x)
           (pixelData(y - 1)(x) + pixelData(y + 1)(x)) / 2
-        else pixelData(y)(x)
+        } else pixelData(y)(x)
       })
     }
     val fixed = (0 until height).map(fix)
@@ -86,8 +91,8 @@ object PSM {
     * @return
     */
   private def normalizeImage(image: DicomImage, min: Float, max: Float): DicomImage = {
-    val scale = (image.maxPixelValue - image.minPixelValue) / (max - min)
-    val offset = max - (min * scale)
+    val scale = (max - min) / (image.maxPixelValue - image.minPixelValue)
+    val offset = max - (image.maxPixelValue * scale)
 
     val pixelDataNormalized = image.pixelData.map(row => row.map(p => (p * scale) + offset))
 
@@ -183,26 +188,48 @@ object PSM {
     al
   }
 
-  private def makeProfiles(al: AttributeList): (Elem, String) = {
+  private case class Column(name: String, valueList: Seq[Float]) {}
+
+  private def makeProfiles(al: AttributeList): Seq[Column] = {
     val image = new DicomImage(al)
-    val xValueList = (1 to image.width).map(_.toDouble)
 
     val numPix = 10
-    val numPixD = numPix.toDouble
+    val numPixD = numPix.toFloat
 
-    val transverse: Seq[Double] = {
+    val transverse = {
+      val v = image.columnSums.map(p => p / image.height)
+      Column("Transverse Full Image", v)
+    }
+
+    val transverseCenter = {
       val rect = new Rectangle(0, (image.height - numPix) / 2, image.width, numPix)
       val sub = image.getSubimage(rect)
-      sub.columnSums.map(p => p / numPixD)
+      val v = sub.columnSums.map(p => p / numPixD)
+      Column(s"Transverse Center $numPix Pixels", v)
     }
 
-    val axial: Seq[Double] = {
+    val axial = {
+      val v = image.rowSums.map(p => p / image.width)
+      Column("Axial Full Image", v)
+    }
+
+    val axialCenter = {
       val rect = new Rectangle((image.width - numPix) / 2, 0, numPix, image.height)
       val sub = image.getSubimage(rect)
-      sub.columnSums.map(p => p / numPixD)
+      val v = sub.rowSums.map(p => p / numPixD)
+      Column(s"Axial Center $numPix Pixels", v)
     }
 
-    WedgeHTML.lineColor
+    val histogram = image.binnedHistogram(image.width)
+
+    val histogramValue = Column("Histogram Value", histogram.map(_.value))
+    val histogramCount = Column("Histogram Count", histogram.map(_.count.toFloat))
+
+    Seq(transverse, transverseCenter, axial, axialCenter, histogramCount, histogramValue)
+  }
+
+  /*
+    val xValueList = (1 to image.width).map(_.toDouble)
     val chart = new C3Chart( //
       xAxisLabel = "CU", //
       xDataLabel = "CU", //
@@ -211,8 +238,71 @@ object PSM {
       yDataLabel = "Level", //
       yValues = Seq(transverse, axial) //
     ) //
+   */
 
-    (chart.html, chart.javascript)
+  private def makeCsv(beforeAl: AttributeList, afterAl: AttributeList): Unit = {
+
+    val before = makeProfiles(beforeAl)
+    val after = makeProfiles(afterAl)
+
+    val csvText = {
+      before.map(b => "Before " + b.name).mkString(",")
+      after.map(b => "After " + b.name).mkString(",")
+
+      val header: String = before + "," + after
+
+      val data: String = {
+        val size = before.head.valueList.size
+        val both = before ++ after
+
+        def makeRow(index: Int): String = {
+          def toText(value: Float): String = {
+            if (value.round == value)
+              value.round.toString
+            else
+              value.toString
+          }
+          both
+            .map(c =>
+              toText(
+                {
+                  Trace.trace(s"$index   ${c.name}  ${c.valueList.size}")
+                  c.valueList(index)
+                }
+              )
+            )
+            .mkString(",")
+        }
+
+        val rowText = (0 until size).map(makeRow).mkString(",")
+        rowText
+      }
+
+      s"$header,$data"
+    }
+
+    val psmCsvFile = new File(outDir, "PSM.csv")
+    Util.writeFile(psmCsvFile, csvText)
+    println(s"Wrote file ${psmCsvFile.getAbsolutePath}")
+
+  }
+
+  private def fixZeroes(image: DicomImage): DicomImage = {
+    def makeRow(y: Int): IndexedSeq[Float] = {
+      def fixPixel(x: Int): Float = {
+        val p = image.get(x, y)
+        if (p <= 0) {
+          val mean = (image.get(x - 1, y) + image.get(x + 1, y)) / 2
+          mean
+        } else
+          p
+      }
+
+      val row = (0 until image.width).map(fixPixel)
+      row
+    }
+    val pixelData = (0 until image.height).map(makeRow)
+    new DicomImage(pixelData)
   }
 
   def main(args: Array[String]): Unit = {
@@ -223,43 +313,68 @@ object PSM {
 
     val wdDicomFile = new DicomFile(new File(dir, "RI.zzz_EpidPerformanceCheck.WD-0.dcm"))
     val beam6x_FFDicomFile = new DicomFile(new File(dir, "6x_FF.dcm"))
+
     val psmFile = new File(dir, "PSM_6x_CMN_230520.mat")
 
     println("\n---------------------\n")
 
     val wdImage = new DicomImage(wdDicomFile.attributeList.get)
     val beam6x_FFImage = new DicomImage(beam6x_FFDicomFile.attributeList.get)
+
+    if (true) {
+      for (x <- 0 until 1190)
+        for (y <- 0 until 1190) {
+          val p = beam6x_FFImage.get(x, y)
+          if (p <= 0) {
+            Trace.trace(s"zero pixel at x: $x    y: $y")
+          }
+        }
+    }
+
     val psmImage = psmToDicomImage(psmFile)
 
-    val wdZBeam6x_FF = {
+    val wdXBeam6x_FF = {
       val pixelData = beam6x_FFImage.pixelData.zip(wdImage.pixelData).map(tw => tw._1.zip(tw._2).map(pair => pair._1 * pair._2))
       new DicomImage(pixelData)
     }
 
-    val psmCorrected = {
-      val pixelData = wdZBeam6x_FF.pixelData.zip(psmImage.pixelData).map(tw => tw._1.zip(tw._2).map(pair => pair._1 / pair._2))
+    val wdXbeam6x_ffDivPSM = {
+      def div(pair: (Float, Float)): Float = {
+        val result = pair._1 / pair._2
+        val d1 = pair._1.toDouble
+        val d2 = pair._2.toDouble
+        val d = d1 / d2
+        val diffD = result - d
+        val f: Float = d.toFloat
+        val diffF = result - f
+        result
+      }
+      val pixelData = wdXBeam6x_FF.pixelData.zip(psmImage.pixelData).map(tw => tw._1.zip(tw._2).map(div))
       new DicomImage(pixelData)
     }
 
-    val psmCorrectedAndNormalized = normalizeImage(psmImage, beam6x_FFImage.minPixelValue, beam6x_FFImage.maxPixelValue)
+    val wdXbeam6x_ffDivPSMNorm = normalizeImage(wdXbeam6x_ffDivPSM, beam6x_FFImage.minPixelValue, beam6x_FFImage.maxPixelValue)
 
-    val finalDicom = makeDicom(psmCorrectedAndNormalized, beam6x_FFDicomFile.attributeList.get)
+    val finalDicom = makeDicom(wdXbeam6x_ffDivPSMNorm, beam6x_FFDicomFile.attributeList.get)
 
     val roundTrip = new DicomImage(finalDicom)
 
-    Trace.trace("\nwd:     " + middleOf(wdImage))
-    Trace.trace("\nbeam6x_FF:     " + middleOf(beam6x_FFImage))
-    Trace.trace("\nwd x beam6x_FF:     " + middleOf(wdZBeam6x_FF))
-    Trace.trace("\npsm:     " + middleOf(psmImage))
-    Trace.trace("\npsmCorrected:     " + middleOf(psmCorrectedAndNormalized))
-    Trace.trace("\nroundTrip:     " + middleOf(roundTrip))
+    println("\nbeam6x_FF:     " + middleOf(beam6x_FFImage))
+    println("\nwd:     " + middleOf(wdImage))
+    println("\nwd x beam6x_FF:     " + middleOf(wdXBeam6x_FF))
+    println("\npsm:     " + middleOf(psmImage))
+    println("\n(wd x beam6x_FF) / psm:     " + middleOf(wdXbeam6x_ffDivPSM))
+    println("\n(wd x beam6x_FF) / psm and then normalized:     " + middleOf(wdXbeam6x_ffDivPSMNorm))
+    println("\nroundTrip:     " + middleOf(roundTrip))
 
     savePng(wdImage, "wdImage")
     savePng(beam6x_FFImage, "beam6x_FFImage")
-    savePng(wdZBeam6x_FF, "wdXbeam6x_FF")
+    savePng(wdXBeam6x_FF, "wdXbeam6x_FF")
     savePng(psmImage, "psmImage")
-    savePng(psmCorrectedAndNormalized, "psmCorrectedAndNormalized")
+    savePng(wdXbeam6x_ffDivPSMNorm, "psmCorrectedAndNormalized")
     savePng(roundTrip, "roundTrip")
+
+    // makeCsv(beam6x_FFDicomFile.attributeList.get, finalDicom)
 
     val finalDicomFile = new File(outDir, "psmCorrectedAndNormalized.dcm")
     DicomUtil.writeAttributeListToFile(finalDicom, finalDicomFile, "PSM")
