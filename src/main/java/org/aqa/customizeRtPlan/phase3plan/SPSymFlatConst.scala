ba69cc73ba0b912985ce28ac99b8ba1353760e7f
Jim Irrer <irrer@umich.edu>
2023-12-26 19:53:12 -0500

    Fixed SymFlatConst so that it would make beams.
    If the machine supports more energies, then SymFlatConst beams have to be made for them.

--- src/main/java/org/aqa/customizeRtPlan/phase3plan/SPSymFlatConst.scala
+++ src/main/java/org/aqa/customizeRtPlan/phase3plan/SPSymFlatConst.scala
@@ -3,6 +3,7 @@ package org.aqa.customizeRtPlan.phase3plan
 import com.pixelmed.dicom.AttributeList
 import org.aqa.Config
 import org.aqa.Util
+import org.aqa.db.MachineBeamEnergy
 
 import scala.xml.Elem
 
@@ -11,10 +12,65 @@ class SPSymFlatConst(metaData: SPMetaData, beamList: Seq[Beam]) extends SubProce
   override val name = "Symmetry Flatness"
 
   override val abbreviation: String = "Sym Flat"
-  def toCdBeam(beam: Beam): Elem = ???
+
+  /**
+    * Make a list of machine beam energies that are not covered (defined) in the prototype plan.
+    * @param coveredBeamList SymFlat beams that are defined in the prototype plan.
+    * @return List of energies that this machine supports but are not represented in the coveredBeamList.
+    */
+  private def makeUnusedEnergyList(coveredBeamList: Seq[Selection]) = {
+
+    def energySig(e: MachineBeamEnergy): String = {
+      Seq(e.photonEnergy_MeV, e.maxDoseRate_MUperMin, e.fffEnergy_MeV).flatten.map(_.toString).mkString(" | ")
+    }
+
+    def energySignature(beam: Beam): String = energySig(beam.beamEnergy)
+
+    // list of
+    val coveredEnergyList = coveredBeamList.flatMap(_.beamList).groupBy(energySignature).values.map(_.head.beamEnergy)
+
+    def inKnownList(e: MachineBeamEnergy): Boolean = {
+      val eSig = energySig(e)
+      coveredEnergyList.exists(cov => energySig(cov).equals(eSig))
+    }
+
+    val missingEnergyList = metaData.beamEnergyList.filterNot(inKnownList)
+
+    missingEnergyList
+  }
+
+  private def toBeam(beamEnergy: MachineBeamEnergy, protoypeBeam: Beam): Beam = {
+    val beamName = {
+      val prefix = protoypeBeam.beamName.take(4) // example beam names: J20G0-6F, J18G0-6X
+      val gantryAngle = protoypeBeam.gantryAngle_roundedDeg
+      val kvp = {
+        val e = beamEnergy.photonEnergy_MeV.get
+        if (e.round == e)
+          e.round.toString
+        else
+          Util.fmtDbl(beamEnergy.photonEnergy_MeV.get)
+      }
+      val fff = if (beamEnergy.isFFF) "F" else "X"
+      s"$prefix$gantryAngle-$kvp$fff"
+    }
+
+    Beam(protoypeBeam.prototypeBeam, beamName, beamEnergy)
+  }
+
+  private def toSelection(beam: Beam): Selection = Selection(this, beam.beamName, Seq(beam))
+
+  private def makeNewBeamList(coveredBeamList: Seq[Selection]): Seq[Selection] = {
+    val unusedEnergyList = makeUnusedEnergyList(coveredBeamList)
+
+    def isFlood(selection: Selection): Boolean = selection.beamList.head.beamName.toLowerCase().contains("flood")
+
+    val prototypeBeam = coveredBeamList.filterNot(isFlood).minBy(_.beamList.head.gantryAngle_roundedDeg).beamList.head
+
+    val beamList = unusedEnergyList.map(e => toBeam(e, prototypeBeam))
+    beamList.map(toSelection)
+  }
 
   override def selectionList: Seq[Selection] = {
-    def toSelection(beam: Beam): Selection = Selection(this, beam.beamName, Seq(beam))
     val margin = Config.SymmetryAndFlatnessDiameter_mm / 2
     val max = {
       val m = Config.SymmetryAndFlatnessPointList.flatMap(p => Seq(p.x_mm, p.y_mm)).flatMap(c => Seq(c + margin, c - margin))
@@ -23,11 +79,13 @@ class SPSymFlatConst(metaData: SPMetaData, beamList: Seq[Beam]) extends SubProce
 
     def ok(beam: Beam) = Util.minCenteredFieldBeam(beam.prototypeBeam, max)
     val list = beamList.filter(ok).map(toSelection)
-    list
+
+    val newBeamList = makeNewBeamList(list)
+
+    list ++ newBeamList
   }
 
   override def setBeamList(beamList: Seq[Beam]): Elem = {
-    beamList.map(toCdBeam)
     ???
   }
 
