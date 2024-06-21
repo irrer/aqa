1c9d4afb075f66b9acab448806b33c6ef657b9e0
Jim Irrer <irrer@umich.edu>
2024-06-21 18:20:55 -0400

    Code cleanup.

--- src/test/java/aqa/test/TestFocalSpotAnalyze.scala
+++ src/test/java/aqa/test/TestFocalSpotAnalyze.scala
@@ -16,19 +16,9 @@
 
 package aqa.test
 
-import com.pixelmed.dicom.AttributeList
-import com.pixelmed.dicom.DicomFileUtilities
-import edu.umro.ScalaUtil.FileUtil
-import edu.umro.ScalaUtil.Trace
-import org.aqa.Util
-import org.aqa.webrun.focalSpot.FSSet
-import org.aqa.Config
 import org.aqa.Logging
-import org.aqa.webrun.focalSpot.FSMeasure
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
-
-import java.io.File
 
 /**
   * Test the FSAnalyze.measure function, which does the edge measurements for focal spot.
@@ -36,77 +26,6 @@ import java.io.File
 
 class TestFocalSpotAnalyze extends FlatSpec with Matchers with Logging {
   "Focal Spots" should "be good" in {
-
-    val outDir = new File(new File("target"), "FocalSpot")
-    FileUtil.deleteFileTree(outDir)
-    outDir.mkdirs()
-
-    def showResult(inDir: File, fsSet: FSSet): Unit = {
-
-      val outSubDir = new File(outDir, inDir.getName)
-      outSubDir.mkdirs()
-
-      def save(fsMeasure: FSMeasure): Unit = {
-        val outFileName = fsMeasure.beamName + ".png"
-        val outFile = new File(outSubDir, outFileName)
-        Util.writePng(fsMeasure.bufferedImage, outFile)
-        println("Wrote file " + outFile.getAbsolutePath)
-      }
-
-      save(fsSet.jaw090)
-      save(fsSet.jaw270)
-      save(fsSet.mlc090)
-      save(fsSet.mlc270)
-
-      Trace.trace("X Alignment: " + fsSet.focalSpotAlignmentX_mm)
-      Trace.trace("Y Alignment: " + fsSet.focalSpotAlignmentY_mm)
-      println
-    }
-
-    def readDicomFile(file: File): Option[AttributeList] = {
-      if (DicomFileUtilities.isDicomOrAcrNemaFile(file)) {
-        try {
-          val al = new AttributeList
-          al.read(file)
-          Some(al)
-        } catch {
-          case _: Throwable => None
-        }
-      } else
-        None
-    }
-
-    def doMlc(inDir: File, alList: Seq[AttributeList]): Unit = {
-
-      println("\n\nProcessing " + inDir.getAbsolutePath)
-      val rtplan = alList.find(Util.isRtplan).get
-      val rtimageList = alList.filter(Util.isRtimage)
-
-      ??? // TODO
-      // val fsSetList = FSAnalysis(rtplan, rtimageList).setList
-
-      // fsSetList.foreach(fsSet => showResult(inDir, fsSet))
-
-    }
-
-    /** Main directory containing test data. */
-    val mainDir = new File("""src\test\resources\TestFocalSpot""")
-
-    Config.validate
-
-    /** Individual test cases. */
-    val dirList = Util.listDirFiles(mainDir)
-
-    def toInput(dir: File): (File, Seq[AttributeList]) = {
-      val alList = Util.listDirFiles(dir).flatMap(readDicomFile)
-      (dir, alList)
-    }
-
-    val list = dirList.map(toInput)
-    println("\nProcessing directories:")
-    list.foreach(da => doMlc(da._1, da._2))
-
-    true should be(true)
-    println("\nDone.")
+    //
   }
 }
