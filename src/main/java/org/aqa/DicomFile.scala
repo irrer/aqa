c3f12f97b4d1903ccb4ec2c272372400984d8ef8
Jim Irrer <irrer@umich.edu>
2024-07-24 08:03:35 -0400

    Added al convenience function and did code cleanup.

--- src/main/java/org/aqa/DicomFile.scala
+++ src/main/java/org/aqa/DicomFile.scala
@@ -27,18 +27,20 @@ import java.io.File
 
 case class DicomFile(file: File) extends Logging {
   private lazy val readResult = Util.readDicomFile(file)
-  lazy val valid = readResult.isRight
+  lazy val valid: Boolean = readResult.isRight
   lazy val attributeList: Option[AttributeList] = if (readResult.isRight) Some(readResult.right.get) else None
   lazy val error: Option[Throwable] = if (readResult.isLeft) Some(readResult.left.get) else None
 
-  lazy val standardImage: Option[BufferedImage] = {
+  def al: AttributeList = attributeList.get
+
+  private lazy val standardImage: Option[BufferedImage] = {
     if (valid && DicomUtil.isImageStorage(attributeList.get))
       Some(ConsumerFormatImageMaker.makeEightBitImage(attributeList.get))
     else
       None
   }
 
-  lazy val maxContrastImage: Option[BufferedImage] = {
+  private lazy val maxContrastImage: Option[BufferedImage] = {
     if (valid && DicomUtil.isImageStorage(attributeList.get)) {
       val pixelList = attributeList.get.get(TagFromName.PixelData).getShortValues.map(s => s & 0xffff).toIndexedSeq
       val lo = pixelList.min
@@ -49,7 +51,7 @@ case class DicomFile(file: File) extends Logging {
       val width = attributeList.get.get(TagFromName.Columns).getIntegerValues.head
       val image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
 
-      for (xy <- (0 until (width * height))) {
+      for (xy <- 0 until (width * height)) {
         val x = xy % width
         val y = xy / width
         val brightness = ((pixelList(xy) - lo) * ratio).floor.toInt & 0xff
@@ -65,10 +67,9 @@ case class DicomFile(file: File) extends Logging {
     contrastModel match {
       case DicomFile.ContrastModel.standard    => standardImage
       case DicomFile.ContrastModel.maxContrast => maxContrastImage
-      case _ => {
+      case _ =>
         logger.error(fmtEx(new RuntimeException("Invalid colorScheme: " + contrastModel)))
         None
-      }
     }
   }
 
@@ -79,23 +80,21 @@ case class DicomFile(file: File) extends Logging {
 
   def reDir(dir: File) = new DicomFile(Util.reDir(file, dir))
 
-  lazy val originalDicomImage: Option[DicomImage] = {
+  private lazy val originalDicomImage: Option[DicomImage] = {
     attributeList match {
-      case Some(al) => Some(new DicomImage(attributeList.get))
-      case _        => None
+      case Some(_) => Some(new DicomImage(attributeList.get))
+      case _       => None
     }
   }
 
   private lazy val badPixelList: Option[Seq[DicomImage.PixelRating]] = {
     originalDicomImage match {
-      case Some(odi) => {
+      case Some(odi) =>
         val numPixels = odi.width * odi.height
         val million = 1000.0 * 1000
-        val sampleSize = ((Config.BadPixelSamplePerMillion / million) * numPixels).round.toInt
         val maxBadPixels = ((Config.MaxEstimatedBadPixelPerMillion / million) * numPixels).round.toInt
         val badPixels = odi.identifyBadPixels(maxBadPixels, Config.BadPixelStdDev, Config.BadPixelMaximumPercentChange, Util.badPixelRadius(attributeList.get), Config.BadPixelMinimumDeviation_CU)
         Some(badPixels.filter(bp => bp.rating > 100)) // TODO filter?
-      }
       case _ => None
     }
   }
@@ -111,12 +110,9 @@ case class DicomFile(file: File) extends Logging {
 object DicomFile extends Logging {
 
   object ContrastModel extends Enumeration {
-    val standard = Value
-    val maxContrast = Value
+    val standard: Value = Value
+    val maxContrast: Value = Value
   }
-
-  /** Lookup table for brightness -> color. */
-  private lazy val rgbTableBW = (0 until 256).map(b => (b << 16) + (b << 8) + b)
 
   /** Lookup table for brightness -> color. */
   private lazy val rgbTable = (0 until 256).map(b => (b << 8) + b)
