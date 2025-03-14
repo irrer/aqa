bada3c4a27f747ed7a48da496116937c35ad57fa
Jim Irrer <irrer@umich.edu>
2025-03-13 20:32:51 -0400

    Started work on PSM beam correction

--- src/main/java/org/aqa/webrun/psm/PSMBeamAnalysis.scala
+++ src/main/java/org/aqa/webrun/psm/PSMBeamAnalysis.scala
@@ -6,168 +6,39 @@ import edu.umro.ImageUtil.DicomImage
 import edu.umro.ImageUtil.ImageUtil
 import edu.umro.ImageUtil.IsoImagePlaneTranslator
 import edu.umro.ScalaUtil.DicomBeam
+import edu.umro.ScalaUtil.DicomUtil
 import edu.umro.ScalaUtil.Trace
 import org.aqa.Logging
 import org.aqa.Util
 import org.aqa.webrun.phase2.MeasureTBLREdges
-import org.aqa.Config
-import org.aqa.db.PSMBeam
 import org.aqa.webrun.ExtendedData
 
 import java.awt.Point
 import java.awt.geom.Point2D
 import java.awt.image.BufferedImage
-import java.awt.Rectangle
 import javax.vecmath.Point2i
 
-
 /**
- *
- * @param rtplan       DICOM RTPLAN for delivering beams.
- * @param extendedData Metadata
- * @param trans        For translating between pixel and isoplane coordinates.
- * @param rtimage      Beam image.
- */
+  *
+  * @param rtplan       DICOM RTPLAN for delivering beams.
+  * @param extendedData Metadata
+  * @param trans        For translating between pixel and isoplane coordinates.
+  * @param rtimage      Beam image.
+  */
 case class PSMBeamAnalysis(rtplan: AttributeList, extendedData: ExtendedData, trans: IsoImagePlaneTranslator, rtimage: AttributeList) extends Logging {
 
-
   /**
-   * Find the center of mass of the beam.
-   *
-   * @param dicomImage Contains pixel array.
-   * @param planBeam   Specification of how to deliver the beam.  Extracted from the RTPLAN.
-   * @param beamName   Name of beam.
-   * @return Coordinates of center of mass of the beam in pixel coordinates.
-   */
-  private def findCenter_pix(dicomImage: DicomImage, planBeam: DicomBeam, beamName: String): Point2D.Double = {
-
-    /* Build a message to show the results of intermediate calculations in the log. */
-    val msg: StringBuffer = new StringBuffer("%-10s".format(beamName))
-
-    def fmtPoint(point: Point2D.Double): String = {
-      def df(d: Double) = "%14.8f".format(d)
-
-      s"${df(point.getX)} , ${df(point.getY)} "
-    }
-
-    /**
-     * Use the whole image to find the coarse (approximate) center of the field.
-     *
-     * A horizontal and vertical profile are made.  For each, the mean value is
-     * calculated and then all values that are below that are set to 0.  By doing
-     * this, the background is ignored, which makes it very robust.  Because it
-     * also ignores some of the penumbra, it is also less accurate, but serves well
-     * as a first approximation.
-     */
-    def findCoarseCenter(): Point2D.Double = {
-
-      def findCenterOfProfile(profile: Seq[Float]): Float = {
-        val mid = (profile.max + profile.min) / 2
-        val zero = 0.toFloat
-        // set values that are below the mean to zero
-        val prof = profile.map(v => if (v < mid) zero else v)
-        val center = ImageUtil.centerOfMass(prof.toIndexedSeq)
-        center
-      }
-
-      val xCenterCoarse_pix = findCenterOfProfile(dicomImage.columnSums)
-      val yCenterCoarse_pix = findCenterOfProfile(dicomImage.rowSums)
-
-      val coarseCenter_pix = new Point2D.Double(xCenterCoarse_pix.toDouble, yCenterCoarse_pix.toDouble)
-      msg.append(s"\n    coarse: ${fmtPoint(coarseCenter_pix)}    ")
-      coarseCenter_pix
-    }
-
-    /**
-     * Find the center of mass of the beam by using an AOI (area of interest) shaped as two narrow
-     * rectangles  spanning the beam, one vertical and the other horizontal.
-     *
-     * The rectangle must be narrow because some beams may have an artifact in the corner (when the
-     * beam is in the corner), and the only way (I know of) to handle this is to stay away from it.
-     *
-     * @param center_pix   Use this as the center of the AOI in pixel coordinates.
-     * @param thickness_mm Specifies how wide the rectangle should be in mm.
-     * @return Center in pixel coordinates.
-     */
-    def findFineCenter_pix(center_pix: Point2D.Double, thickness_mm: Double): Point2D.Double = {
-
-      val xC = center_pix.getX
-      val yC = center_pix.getY
-
-      // translate thickness to pixels and guarantee that it is at least one pixel wide or tall.
-      val vertThickness_pix = Math.max(trans.iso2PixDistY(thickness_mm).toInt, 1)
-      val horzThickness_pix = Math.max(trans.iso2PixDistX(thickness_mm).toInt, 1)
-
-      // Width and height of area to measure.  This will span the entire beam plus the penumbra.
-      val xFieldSize_mm = (planBeam.x2Jaw.get - planBeam.x1Jaw.get) + Config.PenumbraThickness_mm
-      val yFieldSize_mm = (planBeam.y2Jaw.get - planBeam.y1Jaw.get) + Config.PenumbraThickness_mm
-
-      // Convert to pix coordinates.
-      val xFieldSize_pix = trans.iso2PixDistX(xFieldSize_mm)
-      val yFieldSize_pix = trans.iso2PixDistY(yFieldSize_mm)
-
-      // --------------------------------------------------------------------------------
-
-      // Find the X coordinate using a wide, short, AOI.
-      val xMaxRadius_pix = Math.min(xC, dicomImage.width - xC)
-      val xRadius_pix = Math.min(xFieldSize_pix / 2, xMaxRadius_pix)
-      if (xRadius_pix != (xFieldSize_pix / 2))
-        Trace.trace(s"$beamName shortened X AOI: $xRadius_pix    instead of   ${xFieldSize_pix / 2}")
-      val xFieldPos_pix = (xC - xRadius_pix).toInt
-      val horzRectangle = new Rectangle(xFieldPos_pix, (yC - (vertThickness_pix / 2.0)).toInt, (xRadius_pix * 2).toInt, vertThickness_pix)
-      val xSubImageFine = dicomImage.getSubimage(horzRectangle)
-      val xCenterOfMassFine_pix = ImageUtil.centerOfMass(xSubImageFine.columnSums) + xFieldPos_pix
-
-      // --------------------------------------------------------------------------------
-
-      // Find the Y coordinate using a tall, skinny, AOI.
-      val yMaxRadius_pix = Math.min(yC, dicomImage.height - yC)
-      val yRadius_pix = Math.min(yFieldSize_pix / 2, yMaxRadius_pix)
-      if (yRadius_pix != (yFieldSize_pix / 2))
-        Trace.trace(s"$beamName shortened Y AOI: $yRadius_pix    instead of   ${yFieldSize_pix / 2}")
-      val yFieldPos_pix = (yC - yRadius_pix).toInt
-      val vertRectangle = new Rectangle((xC - (horzThickness_pix / 2.0)).toInt, yFieldPos_pix, horzThickness_pix, (yRadius_pix * 2).toInt)
-      val ySubImageFine = dicomImage.getSubimage(vertRectangle)
-      val yCenterOfMassFine_pix = ImageUtil.centerOfMass(ySubImageFine.rowSums) + yFieldPos_pix
-
-      // --------------------------------------------------------------------------------
-
-      val centerFine_pix = new Point2D.Double(xCenterOfMassFine_pix, yCenterOfMassFine_pix)
-
-      // Shows how different the initial location is from the new one.
-      val dist = centerFine_pix.distance(center_pix)
-      // Build a message for the log.
-      msg.append(s"\n      dist: ${"%f11.8".format(dist)}   ${fmtPoint(centerFine_pix)} ")
-
-      centerFine_pix
-    }
-
-
-    // make a band of pixel this thick in mm.
-    val thickness_mm = 8.0
-
-    val coarseCenter_pix = findCoarseCenter() // start with the more robust but less accurate method.
-    val center1_pix = findFineCenter_pix(coarseCenter_pix, thickness_mm / 4) // use a very narrow AOI to avoid possible artifacts.
-    val center2_pix = findFineCenter_pix(center1_pix, thickness_mm / 2) // use a less narrow AOI because we're probably farther from an artifact.
-    val center3_pix = findFineCenter_pix(center2_pix, thickness_mm) // use a less narrow AOI because we're probably farther from an artifact.
-
-    logger.info(s"Beam centering progression: $msg")
-
-    center3_pix
-  }
-
-  /**
-   * Attempt to measure the position of each of the four edges.  This is not really necessary, but the data is
-   * available so we might as well analyze and save it in case there is a potential use.
-   *
-   * In some cases there are artifacts in the images which make them impossible to
-   *
-   * @param center_mm  Center in isoplane in mm.
-   * @param rtplanBeam Beam from RTPLAN.
-   * @param dicomImage Image pixels.
-   * @param beamName   Name of beam.
-   * @return
-   */
+    * Attempt to measure the position of each of the four edges.  This is not really necessary, but the data is
+    * available so we might as well analyze and save it in case there is a potential use.
+    *
+    * In some cases there are artifacts in the images which make them impossible to
+    *
+    * @param center_mm  Center in isoplane in mm.
+    * @param rtplanBeam Beam from RTPLAN.
+    * @param dicomImage Image pixels.
+    * @param beamName   Name of beam.
+    * @return
+    */
   private def measureEdges(center_mm: Point2D.Double, rtplanBeam: DicomBeam, dicomImage: DicomImage, beamName: String): Option[MeasureTBLREdges.AnalysisResult] = {
     // @formatter:off
     val topPlanned_mm    = center_mm.getY - rtplanBeam.y2Jaw.get
@@ -200,6 +71,28 @@ case class PSMBeamAnalysis(rtplan: AttributeList, extendedData: ExtendedData, tr
     result
   }
 
+  /**
+   * Use the planned center as the center of the beam in isoplane coordinates.
+   *
+   * @param planBeam beam.
+   * @return Center of beam.
+   */
+  private def findCenter_iso(planBeam: DicomBeam): Point2D.Double = {
+
+    val RTImagePosition = {
+      val beamAl = DicomUtil.getBeamOfRtimage(planBeam.rtplan, planBeam.rtimage).get
+      DicomUtil.findAllSingle(beamAl, TagByName.RTImagePosition).head.getDoubleValues
+    }
+
+    val x = -(RTImagePosition.head / trans.beamExpansionRatio)
+    val y = RTImagePosition(1) / trans.beamExpansionRatio
+
+    val p_iso = new Point2D.Double(x, y)
+
+    p_iso
+  }
+
+
   def measure(): PSMBeamAnalysisResult = {
 
     // grab part of RTPLAN that specifies this beam
@@ -219,9 +112,9 @@ case class PSMBeamAnalysis(rtplan: AttributeList, extendedData: ExtendedData, tr
     val RescaleSlope = rtimage.get(TagByName.RescaleSlope).getDoubleValues.head
     val RescaleIntercept = rtimage.get(TagByName.RescaleIntercept).getDoubleValues.head
 
-    val center_pix = findCenter_pix(dicomImage, rtplanBeam, beamName)
+    val center_mm = findCenter_iso(rtplanBeam)
 
-    val center_mm = trans.pix2Iso(center_pix)
+    val center_pix = trans.iso2Pix(center_pix)
 
     def pixToCU(coordinate: Point2i): Double = (dicomImage.get(coordinate.getX, coordinate.getY) * RescaleSlope) + RescaleIntercept
 
@@ -235,7 +128,7 @@ case class PSMBeamAnalysis(rtplan: AttributeList, extendedData: ExtendedData, tr
 
     val ms = if (edges.isDefined) Some(edges.get.measurementSet) else None
 
-    val psmBeam = PSMBeam(
+    val psmBeam = org.aqa.db.PSMBeam(
       psmBeamPK = None,
       outputPK = extendedData.output.outputPK.get,
       xCenter_mm = center_mm.getX,
--- src/main/java/org/aqa/webrun/psm/PSMCorrectImage.scala
+++ src/main/java/org/aqa/webrun/psm/PSMCorrectImage.scala
@@ -1,49 +1,59 @@
 package org.aqa.webrun.psm
 
 import edu.umro.ImageUtil.DicomImage
+import edu.umro.ScalaUtil.Trace
 import org.aqa.db.PSMBeam
+import org.aqa.Logging
 
 import scala.annotation.tailrec
 
-case class PSMCorrectImage(psmList: Seq[PSMBeam]) {
+case class PSMCorrectImage(psmList: Seq[PSMBeam]) extends Logging {
+
+  Trace.trace()
 
   /**
     * Put the beams into a 2-dimensional array that is the same as their spacial layout.
     * @return Spatially sorted beams.
     */
-  private def layoutSpatially: Seq[Seq[PSMBeam]] = {
+  private def layoutSpatially(): Seq[Seq[PSMBeam]] = {
 
     case class Row(beamList: Seq[PSMBeam]) {
       def xSorted: Seq[PSMBeam] = beamList.sortBy(_.xCenter_mm)
+
+      override def toString: String = {
+        def b2S(beam: PSMBeam): String = s"""${beam.xCenter_mm.round.formatted("%3d")},${beam.yCenter_mm.round.formatted("%3d")} :: ${beam.mean_cu.formatted("%5.2f")}"""
+        s"${xSorted.map(b2S).mkString("     ")}"
+      }
     }
 
     @tailrec
     def build(beamList: Seq[PSMBeam], rowList: Seq[Row] = Seq()): Seq[Row] = {
 
       /** Centers (either X or Y) must be this close in mm to be considered to be in the same row or column. */
-      val tolerance_mm = 2.5
+      val tolerance_mm = 5.0
 
       /**
-       * Determine if beams are in the same column.
-       * @param a one beam
-       * @param b the other beam
-       * @return True if they are close together in the Y axis.
-       */
+        * Determine if beams are in the same column.
+        * @param a one beam
+        * @param b the other beam
+        * @return True if they are close together in the Y axis.
+        */
       def yProximal(a: PSMBeam, b: PSMBeam): Boolean = (a.yCenter_mm - b.yCenter_mm).abs < tolerance_mm
 
       if (beamList.isEmpty)
         rowList
       else {
-        val sortedByY = beamList.sortBy(_.yCenter_mm)
-        val first = beamList.minBy(_.yCenter_mm)
-
-        val newRow = Row(sortedByY.takeWhile(beam => yProximal(first, beam)))
-
-        build(beamList.drop(newRow.beamList.size), rowList :+ newRow)
+        val inOut = beamList.groupBy(b => yProximal(b, beamList.head))
+        val row = Row(inOut(true).sortBy(_.xCenter_mm))
+        val out: Seq[PSMBeam] = if (inOut.contains(false)) inOut(false) else Seq()
+        build(out, rowList :+ row)
       }
+
     }
 
     val rowList = build(psmList)
+
+    logger.info("List of sorted values:\n" + rowList.mkString("\n"))
 
     val sorted = rowList.sortBy(_.beamList.head.yCenter_mm).map(_.xSorted)
 
@@ -51,7 +61,7 @@ case class PSMCorrectImage(psmList: Seq[PSMBeam]) {
   }
 
   def correctImage(image: DicomImage): DicomImage = {
-    val sorted = layoutSpatially
+    val sorted = layoutSpatially()
 
     new DicomImage(IndexedSeq(IndexedSeq(1.0.toFloat))) // TODO
   }
