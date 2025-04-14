dd311044ca0ae04f928e089496c7dea29ac4cd47
Jim Irrer <irrer@umich.edu>
2025-04-14 08:44:16 -0400

    Process each beam for sym/flat HTML in parallel.

--- src/main/java/org/aqa/webrun/phase2/symmetryAndFlatness/SymmetryAndFlatnessHTML.scala
+++ src/main/java/org/aqa/webrun/phase2/symmetryAndFlatness/SymmetryAndFlatnessHTML.scala
@@ -118,7 +118,7 @@ object SymmetryAndFlatnessHTML extends Logging {
     val html = Phase2Util.wrapSubProcedure(extendedData, dynamicContent, title = "Symmetry and Flatness", status, Some(runScript), runReq.rtimageMap)
     Util.writeFile(mainHtmlFile, html)
 
-    resultList.foreach(rb => SymmetryAndFlatnessBeamProfileHTML.makeDisplay(subDir, extendedData, rb, runReq))
+    resultList.par.foreach(rb => SymmetryAndFlatnessBeamProfileHTML.makeDisplay(subDir, extendedData, rb, runReq))
 
     summary(mainHtmlFile, resultList.size, status)
   }
