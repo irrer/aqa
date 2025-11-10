34e9e0b53cb06ef2edf737d4bf8c03dd6d6b2a05
Jim Irrer <irrer@umich.edu>
2025-11-10 14:26:14 -0500

    Increase limit for warning about an excessively long DB call.

--- src/main/java/org/aqa/db/Db.scala
+++ src/main/java/org/aqa/db/Db.scala
@@ -131,7 +131,7 @@ object Db extends Logging {
     dbSync.synchronized {
 
       /** A database operation that takes longer than this (in ms) is considered to have taken a very long time. */
-      val veryLongTime_ms = 5 * 1000
+      val veryLongTime_ms = 15 * 1000
       try {
         val start = System.currentTimeMillis()
         val dbAction = db.run(op)
